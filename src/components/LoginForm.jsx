108e627381d9bf74490fa4a6cf52cce63a253f44
PH Assis <pieit@live.com>
2021-07-14 20:47:38 -0300

    requisito 5

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router';
 
 export default function LoginForm(props) {
   const { handleSubmit, handleChange, handleDisabled } = props;
-
+  const history = useHistory();
   return (
-    <form className="w-25 login-form " onSubmit={(e) => handleSubmit(e)}>
+    <form className="w-25 login-form " onSubmit={ (e) => handleSubmit(e) }>
       <div className="mb-3">
         <label htmlFor="inputEmail" className="form-label">
           Email address
           <input
-            onChange={(e) => handleChange(e)}
+            onChange={ (e) => handleChange(e) }
             name="email"
-            data-testid="email-input"
+            data-testid="input-gravatar-email"
             type="email"
             className="form-control"
             id="inputEmail"
@@ -24,17 +25,29 @@ export default function LoginForm(props) {
         <label htmlFor="inputPassword" className="form-label">
           Name
           <input
-            onChange={(e) => handleChange(e)}
+            onChange={ (e) => handleChange(e) }
             name="name"
-            data-testid="password-input"
+            data-testid="input-player-name"
             type="text"
             className="form-control"
             id="inputPassword"
           />
         </label>
       </div>
-      <button disabled={handleDisabled()} type="submit" className="btn btn-primary">
-        Entrar
+      <button
+        disabled={ handleDisabled() }
+        data-testid="btn-play"
+        type="submit"
+        className="btn btn-primary"
+      >
+        Jogar
+      </button>
+      <button
+        type="button"
+        data-testid="btn-settings"
+        onClick={ () => (history.push('settings')) }
+      >
+        Configurações
       </button>
     </form>
   );
---
+++ src/pages/Game.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GameScreenHeader from '../components/GameScreenHeader';
+
+function Game() {
+  return (
+    <div>
+      <GameScreenHeader />
+      Tela do jogo
+    </div>
+  );
+}
+
+export default Game;
