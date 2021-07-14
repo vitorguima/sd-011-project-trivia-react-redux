5dfe26b55c049317c505c523b709e2f9f7da60cb
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-07-14 15:21:16 -0300

    Commit inicial requisito 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './trivia.png';
 import './App.css';
 
-export default function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>
-          SUA VEZ
+          teste
         </p>
       </header>
     </div>
-  );
+    )
+  }
 }
