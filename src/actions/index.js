025d44fa18cef8c39afe1cc64fca844ac47da93f
Dário <dario.junior.rs@gmail.com>
2021-07-14 18:49:07 -0300

    Resolve o requisito 02

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
 export const LOGIN = 'LOGIN';
+export const TOKEN = 'TOKEN';
 
 export const loginAction = (email, name) => ({ type: LOGIN, email, name });
+
+const receiveToken = (token) => ({ type: TOKEN, token });
+
+export const fetchToken = () => (dispatch) => (fetch('https://opentdb.com/api_token.php?command=request')
+  .then((response) => response.json())
+  .then((token) => dispatch(receiveToken(token))));
