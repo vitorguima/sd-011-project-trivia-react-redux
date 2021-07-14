d3c9d983c9c0a50489b716c04d44894a1c614d39
PH Assis <pieit@live.com>
2021-07-14 20:59:42 -0300

    req5 commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import { Home, NotFound, TriviaGame } from './pages';
-=======
 import Settings from './components/Settings';
 import { Home, NotFound, Game } from './pages';
->>>>>>> 6dd7913b3d2bfa2adef04be9c4b26f8cadf698fc
 
 export default function App() {
   return (
     <main>
       <Switch>
-<<<<<<< HEAD
         <Route exact path="/" render={(props) => <Home {...props} />} />
-        <Route exact path="/game" render={(props) => <TriviaGame {...props} />} />
+        <Route exact path="/game" render={(props) => <Game {...props} />} />
+        <Route exact path="/settings" component={Settings} />
         <Route component={NotFound} />
-=======
-        <Route exact path="/" render={ (props) => <Home { ...props } /> } />
-        <Route exact path="/game" render={ (props) => <Game { ...props } /> } />
-        <Route exact path="/settings" component={ Settings } />
-        <Route component={ NotFound } />
->>>>>>> 6dd7913b3d2bfa2adef04be9c4b26f8cadf698fc
       </Switch>
     </main>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,3 @@
 export { default as Home } from './Home';
 export { default as NotFound } from './NotFound';
-<<<<<<< HEAD
-export { default as TriviaGame } from './TriviaGame';
-=======
 export { default as Game } from './Game';
->>>>>>> 6dd7913b3d2bfa2adef04be9c4b26f8cadf698fc
