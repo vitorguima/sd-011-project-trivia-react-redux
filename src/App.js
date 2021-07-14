3706dcd403e9122371e5bf09a6879ed1e6979835
KaioHPs <kaiohpsalternativo@gmail.com>
2021-07-14 18:03:49 -0300

    requisitos 4 e 5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './trivia.png';
+// import logo from './trivia.png';
 import { Route } from 'react-router-dom';
 import Play from './pages/Play';
 import './App.css';
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
+        {/* <img src={ logo } className="App-logo" alt="logo" /> */}
         <Route path="/play" component={ Play }/>
       </header>
     </div>
