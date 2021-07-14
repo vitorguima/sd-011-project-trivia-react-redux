f0ca1e4d52f92b7cd1cd4f54705c0c19f5639246
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-07-14 17:58:54 -0300

    add req03

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import LoginForm from '../components/LoginForm';
 
 class Home extends React.Component {
   render() {
+    const { history } = this.props;
     return (
-      <LoginForm />
+      <>
+        <LoginForm />
+
+        <button
+          type="button"
+          data-testid="btn-settings"
+          onClick={ () => history.push('/configuration') }
+        >
+          Configurações
+        </button>
+      </>
     );
   }
 }
 
 export default Home;
+
+Home.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Game from '../pages/Game';
+import Configuration from '../pages/Configuration';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Home } />
-        <Route exact path="/game" component={ Game } />
+        <Route path="/game" component={ Game } />
+        <Route path="/configuration" component={ Configuration } />
       </Switch>
     );
   }
