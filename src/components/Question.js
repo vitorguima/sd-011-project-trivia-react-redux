bb2912684535e0daa292c3367fdf597ecc5f730a
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-07-14 19:55:16 -0300

    cria o componente questions para exibir as perguntas na tela

---
+++ src/components/Question.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Question extends Component {
+  shufleAnswers(right, wrongs) {
+    const max = 4;
+    const random = Math.floor(Math.random() * max);
+    wrongs.splice(random, 0, right);
+    console.log(wrongs);
+    return wrongs;
+  }
+
+  multipleQuestion() {
+    const { questionsArr, currentQuestion } = this.props;
+    const rightAnswer = (
+      <button
+        type="button"
+        data-testid="correct-answer"
+        key="right"
+      >
+        { questionsArr[currentQuestion].correct_answer }
+      </button>);
+    const wrongAnswer = questionsArr[currentQuestion].incorrect_answers
+      .map((item, index) => (
+        <button
+          type="button"
+          key={ `wrong-${index}` }
+          data-testid={ `wrong-answer-${index}` }
+        >
+          { item }
+        </button>
+      ));
+    const shufledAnswer = this.shufleAnswers(rightAnswer, wrongAnswer);
+    return (
+      <div className="answers">
+        { shufledAnswer }
+      </div>
+    );
+  }
+
+  bolleanQuestion() {
+    const { questionsArr, currentQuestion } = this.props;
+    if (questionsArr[currentQuestion].correct_answer) {
+      return (
+        <div className="answers">
+          <button type="button" data-testid="correct-answer">True</button>
+          <button type="button" data-testid="wrong-answer-0">False</button>
+        </div>
+      );
+    }
+    return (
+      <div className="answers">
+        <button type="button" data-testid="wrong-answer-0">True</button>
+        <button type="button" data-testid="correct-answer">False</button>
+      </div>
+    );
+  }
+
+  renderAwnserButtons() {
+    const { questionsArr, currentQuestion } = this.props;
+    if (questionsArr[currentQuestion].type === 'boolean') {
+      return this.bolleanQuestion();
+    }
+    return this.multipleQuestion();
+  }
+
+  render() {
+    const { questionsArr, currentQuestion } = this.props;
+    return (
+      <div>
+        <p data-testid="question-category">
+          {' '}
+          { questionsArr[currentQuestion].category }
+          {' '}
+        </p>
+        <p data-testid="question-text">
+          {' '}
+          { questionsArr[currentQuestion].question }
+          {' '}
+        </p>
+        { this.renderAwnserButtons() }
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  questionsArr: state.questions.questionsArr,
+  currentQuestion: state.questions.currentQuestion,
+});
+
+Question.propTypes = {
+  questionsArr: PropTypes.arrayOf(PropTypes.shape({
+    category: PropTypes.string,
+    correct_answer: PropTypes.string,
+    difficulty: PropTypes.string,
+    question: PropTypes.string,
+    type: PropTypes.string,
+    incorrect_answers: PropTypes.arrayOf(PropTypes.string),
+  })),
+  currentQuestion: PropTypes.number,
+}.isRequired;
+
+export default connect(mapStateToProps)(Question);
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,5 +1,66 @@
+// informações mockadas
 const INNITIAL_STATE = {
-  questionsArr: [],
+  questionsArr: [
+    {
+      category: 'Geography',
+      type: 'boolean',
+      difficulty: 'easy',
+      question: 'Greenland is covered with grass and Iceland covered with ice.',
+      correct_answer: 'False',
+      incorrect_answers: [
+        'True',
+      ],
+    },
+    {
+      category: 'General Knowledge',
+      type: 'multiple',
+      difficulty: 'medium',
+      question: 'What is the last letter of the Greek alphabet?',
+      correct_answer: 'Omega',
+      incorrect_answers: [
+        'Mu',
+        'Epsilon',
+        'Kappa',
+      ],
+    },
+    {
+      category: 'Science & Nature',
+      type: 'multiple',
+      difficulty: 'hard',
+      question: 'How long is a light-year?',
+      correct_answer: '9.461 Trillion Kilometres',
+      incorrect_answers: [
+        '1 AU',
+        '105.40 Earth-years',
+        '501.2 Million Miles',
+      ],
+    },
+    {
+      category: 'Entertainment: Film',
+      type: 'multiple',
+      difficulty: 'medium',
+      question: 'What is the name of the queen&#039;s pet in A Bug&#039;s Life?',
+      correct_answer: 'Aphie',
+      incorrect_answers: [
+        'Flik',
+        'Hopper',
+        'Dot',
+      ],
+    },
+    {
+      category: 'Science & Nature',
+      type: 'multiple',
+      difficulty: 'medium',
+      question: 'How old is the universe?',
+      correct_answer: '13.8 Billion Years',
+      incorrect_answers: [
+        '4.5 Billion Years',
+        '7.9 Billion Years',
+        '16.2 Billion Years',
+      ],
+    },
+  ],
+  currentQuestion: 0,
 };
 
 function questions(state = INNITIAL_STATE, action) {
