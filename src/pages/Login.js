f01db963645d589de8a924023d66b5f75fd1e4d0
marlon307 <marlon-ramosb@hotmail.com>
2021-07-14 18:14:14 -0300

    Rq 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
-  console.log('Ok');
+  const [stateLogin, setStatelogin] = useState({ email: '', userName: '' });
+  const { email, userName } = stateLogin;
+
+  const validateEmailInput = () => {
+    if (email !== '' && userName !== '') {
+      return false;
+    } return true;
+  };
+  const sendValue = ({ target }) => {
+    const { name, value } = target;
+    setStatelogin({
+      ...stateLogin,
+      [name]: value,
+    });
+  };
+
   return (
-    <div>
-      Login
-    </div>
+    <form>
+      <input
+        type="email"
+        name="email"
+        onChange={ sendValue }
+        value={ email }
+        data-testid="input-gravatar-email"
+      />
+      <input
+        type="text"
+        name="userName"
+        onChange={ sendValue }
+        value={ userName }
+        data-testid="input-player-name"
+      />
+      <button
+        type="button"
+        data-testid="btn-play"
+        disabled={ validateEmailInput() }
+      >
+        Iniciar Jogo
+      </button>
+    </form>
   );
 };
 
