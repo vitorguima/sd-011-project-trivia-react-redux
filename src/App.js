a504c8165a9f2440d68d1e2d8c04cec33b0a4a08
Vitor <vitorguima@gmail.com>
2021-07-14 18:37:25 -0300

    requisito 3 finalizado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Settings from './pages/Settings/Settings';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route path="/settings" component={ Settings } />
       </Switch>
     </BrowserRouter>
   );
