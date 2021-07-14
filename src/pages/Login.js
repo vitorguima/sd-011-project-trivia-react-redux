8d0407323daca3616415fab3b8019c48fee275e2
Hamander <1hamander@gmail.com>
2021-07-14 17:09:36 -0300

    Requisito 1 ADD FILES

---
+++ src/pages/Login.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import logo from '../trivia.png';
+
+export default class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      gravatarEmail: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { name, gravatarEmail } = this.state;
+    return (
+      <div className="App-header">
+        <header>
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>
+            SUA VEZ
+          </p>
+        </header>
+        <section>
+          <input
+            data-testid="input-player-name"
+            name="name"
+            type="text"
+            onChange={ this.handleChange }
+          />
+          <input
+            data-testid="input-gravatar-email"
+            name="gravatarEmail"
+            type="email"
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="btn-play"
+            disabled={ !(name && gravatarEmail) }
+            type="button"
+          >
+            Jogar
+          </button>
+        </section>
+      </div>
+    );
+  }
+}
