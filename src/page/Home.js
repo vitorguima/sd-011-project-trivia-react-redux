d576444f752054adfc777f014561e0f73610037a
Jander Liborio <jandernunes14@gmail.com>
2021-07-14 16:10:40 -0400

    req 1 feito os input

--- src/page/Home.js
+++ src/page/Home.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react';
 import logo from '../trivia.png';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+    };
+    this.handleData = this.handleData.bind(this);
+  }
+
+  handleData({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { name, email } = this.state;
     return (
       <div>
         <header className="App-header">
@@ -10,6 +28,31 @@ export default class Home extends Component {
           <p>
             SUA VEZ
           </p>
+          <label htmlFor="name">
+            <input
+              type="text"
+              id="name"
+              data-testid="input-player-name"
+              name="name"
+              onChange={ (value) => this.handleData(value) }
+            />
+          </label>
+          <label htmlFor="email">
+            <input
+              type="email"
+              id="email"
+              data-testid="input-gravatar-email"
+              name="email"
+              onChange={ (value) => this.handleData(value) }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="btn-play"
+            disabled={ !(name && email) }
+          >
+            Jogar
+          </button>
         </header>
       </div>
     );
