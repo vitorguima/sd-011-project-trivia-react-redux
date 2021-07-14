184ea602f2ad03c0494b4c121dda38d8800c5525
Arthur Hermann <arthurhermannc@gmail.com>
2021-07-14 17:25:27 -0300

    req1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import md5 from 'crypto-js/md5';
-import logo from './trivia.png';
 import './App.css';
+import { Switch, Route } from 'react-router';
+import { connect } from 'react-redux';
+import Home from './pages/Home';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-          inicio
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Home } />
+      </Switch>
+    );
+  }
 }
+
+// const mapStateToProps = (state) => ({
+//   logInfo: state.pageReducer.logged,
+// })
+
+// const mapDispatchToProps = (dispatch) => ({
+//   logoff: () => dispatch(logoffAction())
+// })
+
+export default connect(null, null)(App);
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Forms from '../components/Forms';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <Forms />
+      </div>
+
+    );
+  }
+}
+
+export default Home;
