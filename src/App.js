7d73fd9d943508489be0a31c0a1ab49cf58643e5
Thyago Pessoa <flash_thyago@hotmail.com>
2021-07-14 16:44:24 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+// import { Route, Switch } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
+import Login from './components/Login';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
+        <Login />
         <p>
           SUA VEZ
         </p>
