b64a7d0d6a7c8438730b554700acde1f7854cc26
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-07-14 18:00:57 -0300

    Requisito 1

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export const signIn = (payload) => ({
+  type: ADD_USER,
+  payload,
+});
+
+export default signIn;
---
+++ src/pages/Login.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import logo from '../trivia.png';
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      buttonDisabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  validateLogin() {
+    const { name, email } = this.state;
+    const ln = 3;
+    const nameLen = name.length;
+    const reg = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+    if (reg.test(email) && nameLen >= ln) {
+      this.setState({
+        buttonDisabled: false,
+      });
+    } else {
+      this.setState({
+        buttonDisabled: true,
+      });
+    }
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+
+    }, () => this.validateLogin());
+  }
+
+  render() {
+    const { buttonDisabled } = this.state;
+    return (
+      <>
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          {/* <p>
+            SUA VEZ
+          </p> */}
+        </header>
+
+        <form>
+          <label htmlFor="inputName">
+            Nome
+            <input
+              data-testid="input-player-name"
+              name="name"
+              type="text"
+              // value={ nome }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="inputEmail">
+            E-mail
+            <input
+              data-testid="input-gravatar-email"
+              name="email"
+              type="email"
+              // value={ email }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            data-testid="btn-play"
+            type="button"
+            disabled={ buttonDisabled }
+          >
+            Jogar
+          </button>
+        </form>
+      </>
+    );
+  }
+}
