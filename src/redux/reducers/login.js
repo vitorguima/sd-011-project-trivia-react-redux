4d9487b539f5dc69fdae7fe37695012573167d65
Diogo Augusto <diogotrescastro@gmail.com>
2021-07-14 18:44:48 -0300

    atualizaçao do requisito 1

---
+++ src/redux/reducers/login.js
@@ -0,0 +1,4 @@
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+};
---
+++ src/redux/reducers/player.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = {
+  player: {
+    name: '',
+    assertions: '',
+    score: '',
+    gravatarEmail: '',
+  },
+};
