8d10a393286b44c35ed0d1e8d44fb85c4901bbfc
Marco Galindo <marco1408@gmail.com>
2021-07-14 17:30:41 -0300

    requisito 3 finalizado

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
+      <Route path="/settings" component={ Settings } />
     </Switch>
   );
 }
