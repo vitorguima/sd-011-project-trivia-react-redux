16a309dc14e2a7426915ec1f338936544eaea1e0
Diego Ribeiro <dihrm2001@gmail.com>
2021-07-14 16:56:13 -0300

    Requisito 2 completo

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
-export default class Login extends Component {
+class Login extends Component {
   constructor() {
     super();
 
     this.state = {
       name: '',
       email: '',
+      play: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.checkForm = this.checkForm.bind(this);
-    // this.handlePlayButton = this.handlePlayButton.bind(this);
+    this.handlePlayButton = this.handlePlayButton.bind(this);
   }
 
   handleChange({ target }) {
@@ -27,12 +28,24 @@ export default class Login extends Component {
     return name !== '' && email !== '';
   }
 
-  // handlePlayButton() {
-
-  // }
+  async handlePlayButton() {
+    fetch('https://opentdb.com/api_token.php?command=request')
+      .then(res => {
+        res.json()
+          .then(json => {
+            localStorage.setItem('token', json.token);
+            this.setState({ play: true });
+          });
+      });
+  }
 
   render() {
-    const { name, email } = this.state;
+    const { name, email, play } = this.state;
+
+    if (play) {
+      return <Redirect to="/jogo" />
+    }
+
     return (
       <div>
         <h1>Login</h1>
@@ -56,7 +69,7 @@ export default class Login extends Component {
           <button
             type="button"
             data-testid="btn-play"
-            // onClick={ this.handlePlayButton }
+            onClick={ this.handlePlayButton }
             disabled={ !this.checkForm() }
           >
             Jogar
@@ -66,3 +79,5 @@ export default class Login extends Component {
     );
   }
 }
+
+export default Login;
