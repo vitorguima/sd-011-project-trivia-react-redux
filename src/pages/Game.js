e92af8e1e15e494baa385e37160fb6646983a6b8
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-07-14 18:53:40 -0300

    Requisito 2 concluido

---
+++ src/pages/Game.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        Tela do Jogo
+      </div>
+    );
+  }
+}
+
+export default Game;
