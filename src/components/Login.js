12e60b71a06b00ce5dc7edbad77053a79a6a0b07
Raphael <raphaellfpg@gmail.com>
2021-07-14 16:48:05 -0300

    Requisito 3 completo

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Config from './Config';
 
 export default class Login extends Component {
   constructor() {
