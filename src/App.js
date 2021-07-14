d373d8f7434c9afe74334c66bdd5343b9cbee1ef
gabiazevedo <gabiazevedoms@gmail.com>
2021-07-14 19:02:29 -0300

    Adiciona requisito 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import logo from './trivia.png';
 import Login from './pages/Login';
 import GamePage from './pages/GamePage';
-// import logo from './trivia.png';
+import Settings from './pages/Settings';
+import logo from './trivia.png';
 import './App.css';
+
+{ /* <img src={ logo } className="App-logo" alt="logo" /> */ }
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        { /* <img src={ logo } className="App-logo" alt="logo" /> */ }
+        <img src={ logo } className="App-logo" alt="logo" />
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={ Login } />
+            <Route exact path="/settings" component={ Settings } />
             <Route exact path="/gamepage" component={ GamePage } />
           </Switch>
         </BrowserRouter>
