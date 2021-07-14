3e7498acffdcc2275b4081bc4dfb5adbbd55d20b
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-07-14 18:29:44 -0300

    Create Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,19 @@
-// infos: gratar, nome do jogador
+import React from 'react';
+// import { connect } from 'react-redux';
+
+function Header() {
+  // const { player } = this.props;
+  return (
+    <header>
+      <img data-testid="header-profile-picture" src={ player.avatar } alt="avatar" />
+      <h2 data-testid="header-player-name">{ player.name }</h2>
+      <h3>{ player.score }</h3>
+    </header>
+  );
+}
+
+// const mapStateToProps = (state) => ({
+//   player: state.player,
+// });
+
+export default Header; // connect(mapStateToProps, mapDispatchToProps)(Header);
