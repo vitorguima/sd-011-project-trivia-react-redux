50769374eb52c9ee8fd55c8f5ceed591f0b42d17
rubia braga <rubiaemilly29@gmail.com>
2021-07-15 16:48:42 -0300

    muda pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import TelaInicial from './Pages/Inicial';
+import Inicial from './Pages/Inicial';
 import Play from './Pages/Play';
 import Feedback from './Pages/Feedback';
 import Ranking from './Pages/Ranking';
@@ -10,7 +10,7 @@ import Settings from './Pages/Settings';
 export default function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ TelaInicial } />
+      <Route exact path="/" component={ Inicial } />
       <Route exact path="/Play" component={ Play } />
       <Route exact path="/Feedback" component={ Feedback } />
       <Route exact path="/Ranking" component={ Ranking } />
