a5a81f5e8d58d58ffb221f3d1444625d7953edde
Jessica Arruda <sicarruda@gmail.com>
2021-07-15 18:38:14 -0300

    requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,12 +3,26 @@ import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  constructor(props) {
+    super(props);
+    this.message = this.message.bind(this);
+  }
+
+  message() {
+    const localStoragePlayer = JSON.parse(localStorage.getItem('state'));
+    const badAssertions = 3;
+    const answer1 = 'Podia ser melhor...';
+    const answer2 = 'Mandou bem!';
+    return localStoragePlayer.player.assertions < badAssertions ? answer1 : answer2;
+  }
+
   render() {
-    const player = JSON.parse(localStorage.getItem('state'));
+    const localStoragePlayer = JSON.parse(localStorage.getItem('state'));
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
-        <Header score={ player.player.score } />
+        <Header score={ localStoragePlayer.player.score } />
+        <h2 data-testid="feedback-text">{this.message()}</h2>
       </div>
     );
   }
