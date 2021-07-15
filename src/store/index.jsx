adf20a3f1f5eed6dd891a7f8128378ca4356422d
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-07-15 18:51:33 -0300

    aacaba pelo amor de deus

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,13 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from '../reducers';
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(
-    applyMiddleware(thunk),
-  ),
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+  applyMiddleware(thunk),
+));
 
 export default store;
