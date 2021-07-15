e8420e0a947573ccc9247e1c071a1ada0b0e02e6
Gisele Costa <jikinha@gmail.com>
2021-07-15 20:24:27 -0300

    Requisito 18 feito

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <Link to="/">
+          <button data-testid="btn-go-home" type="button">Jogar novamente</button>
+        </Link>
       </div>
     );
   }
