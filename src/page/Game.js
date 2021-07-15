02da6546c1ba720d20315a2023869a11b142f40a
Julio Filizzola <juliofilizzola@hotmail.com>
2021-07-14 22:27:12 -0300

    req5

--- src/page/Game.js
+++ src/page/Game.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 
 class Game extends Component {
   render() {
+    const { players, email } = this.props;
+    const objectsLocalStorage = JSON.parse(localStorage.getItem('state'));
+    const hashGenerator = md5(email).toString();
+
     return (
       <div>
-        TESTE
+        <header>
+          <h3 data-testid="header-player-name">{players}</h3>
+          <img
+            src={ `https://www.gravatar.com/avatar/${hashGenerator}` }
+            alt="Gravatar"
+            data-testid="header-profile-picture"
+          />
+          <p>
+            <span data-testid="header-score">
+              {!objectsLocalStorage ? 0 : objectsLocalStorage.score}
+            </span>
+          </p>
+        </header>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  email: state.home.email,
-  players: state.home.name,
+  email: state.homeReducer.email,
+  players: state.homeReducer.name,
 });
 
 export default connect(mapStateToProps)(Game);
+
+Game.propTypes = {
+  players: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
