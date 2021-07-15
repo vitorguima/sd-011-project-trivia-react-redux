f8ac8d1a7092508e38b1e7877b178a7bee7ec69c
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-07-15 18:55:42 -0300

    add req07

--- src/components/BooleanAnswers.jsx
+++ src/components/BooleanAnswers.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const correctAnswer = 'correct-answer';
 class BooleanAnswers extends React.Component {
+  changeColor({ target }) {
+    const getButtons = target.parentElement.children;
+
+    for (let index = 0; index < getButtons.length; index += 1) {
+      if (getButtons[index].dataset.testid === correctAnswer) {
+        getButtons[index].classList.add('correct');
+      } else {
+        getButtons[index].classList.add('incorrect');
+      }
+    }
+  }
+
   render() {
     const { question } = this.props;
     return (
       <>
         <button
           type="button"
+          onClick={ this.changeColor }
           data-testid={
-            question.correct_answer === 'True' ? 'correct-answer' : 'wrong-answer-0'
+            question.correct_answer === 'True' ? correctAnswer : 'wrong-answer-0'
           }
         >
           VERDADEIRO
         </button>
         <button
           type="button"
+          onClick={ this.changeColor }
           data-testid={
-            question.correct_answer === 'False' ? 'correct-answer' : 'wrong-answer-0'
+            question.correct_answer === 'False' ? correctAnswer : 'wrong-answer-0'
           }
         >
           FALSO
