db391d7acded55d6a073e566820549d928a4c0a0
Vitor <vitorguima@gmail.com>
2021-07-15 19:45:56 -0300

    requisito 13 finalizado

--- src/pages/feedback/Feedback.js
+++ src/pages/feedback/Feedback.js
@@ -2,9 +2,33 @@ import React, { Component } from 'react';
 import Header from '../../components/Header';
 
 class Feedback extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalScore: JSON.parse(localStorage.getItem('state')).player.score,
+    };
+    this.renderFeedback = this.renderFeedback.bind(this);
+  }
+
+  renderFeedback() {
+    const worstScore = 3;
+    const { totalScore } = this.state;
+    if (totalScore < worstScore) {
+      return (
+        <p data-testid="feedback-text">Podia ser melhor...</p>
+      );
+    }
+    return (
+      <p data-testid="feedback-text">Mandou bem!</p>
+    );
+  }
+
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        { this.renderFeedback() }
+      </>
     );
   }
 }
