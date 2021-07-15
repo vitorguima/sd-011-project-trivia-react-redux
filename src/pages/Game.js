8ae66340ab95ecafa43b89b778c75926d6592c9a
Anajulia Brito <juliehindius@gmail.com>
2021-07-15 14:57:10 -0300

    erro bizarro

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getToken, getQuestions } from '../services/api';
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+    };
+  }
+
+  componentDidMount() {
+    getToken().then(getQuestions).then((apiQuestions) => {
+      this.setState({ questions: apiQuestions });
+    });
+  }
+
   render() {
+    const { questions } = this.state;
     return (
       <Header />
     );
