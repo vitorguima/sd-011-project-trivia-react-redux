99260dfd07455a3a305298a8434c8c9be42c86ea
marlon307 <marlon-ramosb@hotmail.com>
2021-07-15 18:28:43 -0300

    Rq 8 update-

---
+++ src/components/componentsGame/ButtonsQuestions.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const ButtonsQuestions = ({ descQuestion, btnEnable }) => {
+  const { incorrect_answers: incorrectAnswers,
+    correct_answer: correctAsw } = descQuestion;
+  const [state, setState] = useState({ correctAnswer: '', wrongAnswer: '' });
+  const setClassName = () => {
+    setState({ correctAnswer: 'correct-answer', wrongAnswer: 'wrong-answer' });
+  };
+  return (
+    <>
+      <button
+        onClick={ () => setClassName() }
+        disabled={ btnEnable }
+        className={ state.correctAnswer }
+        type="button"
+        data-testid="correct-answer"
+      >
+        { correctAsw }
+      </button>
+      { incorrectAnswers.map((incorrects, index) => (
+        <button
+          onClick={ () => setClassName() }
+          disabled={ btnEnable }
+          className={ state.wrongAnswer }
+          type="button"
+          key={ index }
+          data-testid={ `wrong-answer-${index}` }
+        >
+          { incorrects }
+        </button>)) }
+    </>
+  );
+};
+
+export default ButtonsQuestions;
+
+ButtonsQuestions.propTypes = {
+  descQuestion: PropTypes.shape({
+    incorrect_answers: PropTypes.arrayOf(PropTypes.string),
+    correct_answer: PropTypes.string,
+  }).isRequired,
+  btnEnable: PropTypes.bool.isRequired,
+};
---
+++ src/components/componentsGame/QuestionDec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const QuestionDesc = ({ descQuestion }) => {
+  const { category, question } = descQuestion;
+  return (
+    <>
+      <span data-testid="question-category">{ category }</span>
+      <span data-testid="question-text">{ question }</span>
+    </>
+  );
+};
+
+export default QuestionDesc;
+
+QuestionDesc.propTypes = {
+  descQuestion: PropTypes.shape({
+    category: PropTypes.string,
+    question: PropTypes.string,
+  }).isRequired,
+};
