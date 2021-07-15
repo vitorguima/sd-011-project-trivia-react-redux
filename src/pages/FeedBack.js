bc7651feae35401485acca77fd0a1aa29992ba4d
Arthur Hermann <arthurhermannc@gmail.com>
2021-07-15 20:43:19 -0300

    create ambient game

---
+++ src/pages/FeedBack.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// ******PÁGINA FAKE, APENAS PARA APROVAR O FINAL DO REQUISITO 11, MANTENHAM A PÁGINA DE VOCÊS*******
+
+class FeedBack extends Component {
+  render() {
+    return (
+      <h1 data-testid="feedback-text">FeedBack</h1>
+    );
+  }
+}
+
+export default FeedBack;
