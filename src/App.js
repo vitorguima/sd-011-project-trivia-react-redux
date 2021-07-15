7f45b7613dc4a7741d8a5b82955570ca23ace98d
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-07-15 16:03:30 -0300

    Add requirement 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import logo from './trivia.png';
+// import logo from './trivia.png';
 import './App.css';
 import Login from './components/Login';
 import Game from './pages/Game';
@@ -9,14 +9,14 @@ import Settings from './pages/Settings';
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <Switch>
-          <Route path="/settings" render={ () => <Settings /> } />
-          <Route path="/game" render={ () => <Game /> } />
-          <Route exact path="/" render={ (props) => <Login { ...props } /> } />
-        </Switch>
-      </header>
+      </header> */}
+      <Switch>
+        <Route path="/settings" render={ () => <Settings /> } />
+        <Route path="/game" render={ () => <Game /> } />
+        <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+      </Switch>
     </div>
   );
 }
