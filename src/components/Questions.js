44551e2d3f862d5ac4045c63c030ceb50c06c3b1
Matheus Costa <mathws.costag@gmail.com>
2021-07-15 17:06:49 -0300

    Randomiza as alternativas

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Question extends React.Component {
   render() {
-    const { newQuestion } = this.props;
-    const { question, correct_answer, incorrect_answers } = newQuestion;
-    const randonAnswer = [ correct_answer, ...incorrect_answers ];
+    const { newQuestion:
+      { question,
+        correct_answer: correctAnswer,
+        incorrect_answers: incorrectAnswers,
+      } } = this.props;
+    const randomAnswers = [correctAnswer, ...incorrectAnswers]
+      .map((a) => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((a) => a.value);
     return (
-      <h1>
-        { question }
-        
-      </h1>
+      <div>
+        <h1>
+          { question }
+        </h1>
+        {randomAnswers.map((answer, index) => <p key={ index }>{ answer }</p>)}
+      </div>
+
     );
   }
 }
+
+Question.propTypes = {
+  newQuestion: PropTypes.isRequired,
+};
 
 export default Question;
