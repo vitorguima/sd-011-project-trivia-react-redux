c8b2b6d4d263845e01b63d59c24ef3bc0071ebb8
Marcos Mantovani <mvmes23@gmail.com>
2021-07-15 18:29:36 -0300

    req 2 feito, bugs corrigidos, lint corrigido!

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
-        {/* <Route exact path="/" render={ () => <Login /> } /> */}
-        <Route exact path="/">
-          <Login />
-        </Route>
+        <Route exact path="/" render={ () => <Login /> } />
+        <Route exact path="/game" render={ () => <Game /> } />
       </Switch>
     </div>
   );
---
+++ src/components/PlayBtn.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlayBtn extends Component {
+  render() {
+    const { func } = this.props;
+    return (
+      <button
+        type="button"
+        data-testid="btn-play"
+        id="play-btn"
+        onClick={ func }
+      >
+        Jogar
+      </button>
+    );
+  }
+}
+
+export default PlayBtn;
+
+PlayBtn.propTypes = {
+  func: PropTypes.func,
+};
+
+PlayBtn.defaultProps = {
+  func: {},
+};
