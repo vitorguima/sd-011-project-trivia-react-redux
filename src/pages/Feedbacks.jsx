f621205dab7d19a726373e03d28b5cc35472b537
Jean Esteves <jean.esteves2001@gmail.com>
2021-07-15 20:38:59 -0300

    Header feito na parte de feedbacks

--- src/pages/Feedbacks.jsx
+++ src/pages/Feedbacks.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Feedbacks extends Component {
+  constructor() {
+    super();
+    this.state = {
+      score: JSON.parse(localStorage.state).player.score,
+      name: JSON.parse(localStorage.state).player.name,
+    };
+  }
+
   render() {
+    const { score, name } = this.state;
     return (
       <div>
         <p data-testid="feedback-text">Feedback</p>
+        <Header
+          score={ score }
+          name={ name }
+        />
       </div>
     );
   }
