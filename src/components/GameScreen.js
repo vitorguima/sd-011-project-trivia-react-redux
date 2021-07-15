2e54f5f753dd69fed254ccb019b22d2731a1f13d
Diego Ribeiro <dihrm2001@gmail.com>
2021-07-15 18:54:21 -0300

    Requisito 9 feito

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import '../styles/GameScreen.css';
 import Header from './Header';
+
+import { playerScore } from '../actions';
 
 class GameScreen extends React.Component {
   constructor() {
@@ -16,6 +21,7 @@ class GameScreen extends React.Component {
     };
     this.handleAnswer = this.handleAnswer.bind(this);
     this.checkTimer = this.checkTimer.bind(this);
+    this.calculateScore = this.calculateScore.bind(this);
   }
 
   componentDidMount() {
@@ -41,13 +47,50 @@ class GameScreen extends React.Component {
     }
   }
 
-  handleAnswer() {
+  calculateScore(difficulty, timer) {
+    const { updateScore } = this.props;
+    const rightAnswer = 10;
+    let point = 1;
+    const medium = 2;
+    const hard = 3;
+    switch (difficulty) {
+    case 'hard':
+      point = hard;
+      break;
+    case 'medium':
+      point = medium;
+      break;
+    default:
+      break;
+    }
+    const score = rightAnswer + (timer * point);
+    updateScore(score);
+    let data = localStorage.getItem('state');
+    console.log(data);
+    const state = JSON.parse(data);
+    const newState = {
+      player: {
+        ...state.player,
+        score,
+        assertions: state.player.assertions + 1,
+      },
+    };
+    data = JSON.stringify(newState);
+    localStorage.setItem('state', data);
+  }
+
+  handleAnswer(correct) {
+    const { triviaApi: { results }, questionNumber, timer } = this.state;
     const styles = ['wrong-answer', 'correct-answer'];
     this.setState({
       styles,
       disabledButton: true,
     });
     clearTimeout(this.time);
+    if (correct) {
+      console.log('Click antes de calcular');
+      this.calculateScore(results[questionNumber].difficulty, timer);
+    }
   }
 
   checkTimer() {
@@ -57,12 +100,44 @@ class GameScreen extends React.Component {
     }
   }
 
-  render() {
+  renderQuestions() {
     const { triviaApi: { results },
       questionNumber,
       styles,
-      timer,
+      redirect,
       disabledButton } = this.state;
+    return (
+      <>
+        {redirect ? <Redirect to="/feedback" /> : null}
+        <h4 data-testid="question-category">{results[questionNumber].category}</h4>
+        <p data-testid="question-text">{results[questionNumber].question}</p>
+        { results[questionNumber].incorrect_answers.map((answer, index) => (
+          <button
+            type="button"
+            data-testid={ `wrong-answer-${index}` }
+            key={ index }
+            className={ styles[0] }
+            onClick={ () => this.handleAnswer(false) }
+            disabled={ disabledButton }
+          >
+            {answer}
+          </button>
+        ))}
+        <button
+          type="button"
+          data-testid="correct-answer"
+          className={ styles[1] }
+          onClick={ () => this.handleAnswer(true) }
+          disabled={ disabledButton }
+        >
+          {results[questionNumber].correct_answer}
+        </button>
+      </>
+    );
+  }
+
+  render() {
+    const { triviaApi: { results }, timer } = this.state;
     this.checkTimer();
     return (
       <>
@@ -70,29 +145,7 @@ class GameScreen extends React.Component {
         <span>{`Timer: ${timer}`}</span>
         {results ? (
           <div>
-            <h4 data-testid="question-category">{results[questionNumber].category}</h4>
-            <p data-testid="question-text">{results[questionNumber].question}</p>
-            { results[questionNumber].incorrect_answers.map((answer, index) => (
-              <button
-                type="button"
-                data-testid={ `wrong-answer-${index}` }
-                key={ index }
-                className={ styles[0] }
-                onClick={ () => this.handleAnswer() }
-                disabled={ disabledButton }
-              >
-                {answer}
-              </button>
-            ))}
-            <button
-              type="button"
-              data-testid="correct-answer"
-              className={ styles[1] }
-              onClick={ () => this.handleAnswer() }
-              disabled={ disabledButton }
-            >
-              {results[questionNumber].correct_answer}
-            </button>
+            {this.renderQuestions()}
           </div>
         ) : (
           <div>
@@ -105,4 +158,12 @@ class GameScreen extends React.Component {
   }
 }
 
-export default GameScreen;
+const mapDispatchToProps = (dispatch) => ({
+  updateScore: (score) => dispatch(playerScore(score)),
+});
+
+GameScreen.propTypes = {
+  updateScore: PropTypes.func,
+}.isRequired;
+
+export default connect(null, mapDispatchToProps)(GameScreen);
