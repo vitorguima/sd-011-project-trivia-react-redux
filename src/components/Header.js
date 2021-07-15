7f0dfb235be7d2cbaba3f84cc23aae7264daf70d
Diogo Augusto <diogotrescastro@gmail.com>
2021-07-15 16:47:33 -0300

    Atualizaçao do requisito 4

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+export class Header extends Component {
+
+  render() {
+    const { profileImg, profileName, profileScore } = this.props;
+    return (
+      <header>
+        <img
+          src={ `https://www.gravatar.com/avatar/${profilePhoto}` }
+          data-testid="header-profile-picture"
+          alt="User profile"
+        />
+        <span
+          data-testid="header-player-name"
+        >
+          { profileName }
+        </span>
+        <span
+          data-testid="header-score"
+        >
+          { `score: ${profileScore}`}
+        </span>
+
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  profileImg: state.player.gravatarEmail,
+  profileName: state.player.name,
+  profileScore: state.player.score,
+});
+
+// const mapDispatchToProps = {
+//   return;
+// };
+
+export default connect(mapStateToProps)(Header);
+
+Header.propTypes = {
+  profileImg: PropTypes.string.isRequired,
+  profileName: PropTypes.string.isRequired,
+  profileScore: PropTypes.string.isRequired,
+};
