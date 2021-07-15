74f2fee2964a3306a1f3b86d7e386fa553516dac
marlon307 <marlon-ramosb@hotmail.com>
2021-07-15 18:28:27 -0300

    Rq 8 update

--- src/components/componentsGame/RenderQuestion.js
+++ src/components/componentsGame/RenderQuestion.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { rquestQuestions } from '../../actions';
+import ButtonsQuestions from './ButtonsQuestions';
 import ComponentTime from './ComponentTime';
+import QuestionDesc from './QuestionDec';
 // Random questions https://stackoverflow.com/questions/59329807/react-map-function-return-one-item-at-random
 
 const RenderQuestion = () => {
   const dispatch = useDispatch();
   const { questions } = useSelector(({ questionsArray }) => questionsArray);
-  const [enambe, setEnable] = useState(false);
-  const [state, setState] = useState({ correctAnswer: '', wrongAnswer: '' });
+  const [enable, setEnable] = useState(false);
   const renderOptionsQuetion = () => {
     const limitedTime = 1000;
     setTimeout(() => {
@@ -18,40 +19,13 @@ const RenderQuestion = () => {
   useEffect(() => {
     renderOptionsQuetion();
   }, []);
-  const renderLoading = () => <div>Loading...</div>;
-  const setClassName = () => {
-    setState({ correctAnswer: 'correct-answer', wrongAnswer: 'wrong-answer' });
-  };
-  const Button = () => (
-    <button
-      onClick={ () => setClassName() }
-      disabled={ enambe }
-      className={ state.correctAnswer }
-      type="button"
-      data-testid="correct-answer"
-    >
-      { questions[0].correct_answer }
-    </button>
-  );
   const renderResult = () => (
     <div className="question">
       <ComponentTime stateButtonsEnable={ setEnable } />
-      <span data-testid="question-category">{ questions[0].category }</span>
-      <span data-testid="question-text">{ questions[0].question }</span>
-      { Button() }
-      { questions[0].incorrect_answers.map((incorrects, index) => (
-        <button
-          onClick={ () => setClassName() }
-          disabled={ enambe }
-          className={ state.wrongAnswer }
-          type="button"
-          key={ index }
-          data-testid={ `wrong-answer-${index}` }
-        >
-          { incorrects }
-        </button>)) }
+      <QuestionDesc descQuestion={ questions[0] } />
+      <ButtonsQuestions descQuestion={ questions[0] } btnEnable={ enable } />
     </div>);
-  return (questions[0] === undefined ? renderLoading() : renderResult());
+  return (questions[0] === undefined ? <div>Loading...</div> : renderResult());
 };
 
 export default RenderQuestion;
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import RenderQuestion from '../components/componentsGame/RenderQuestion';
 
 import Header from '../components/Header';
 
-const Game = () => {
-  const { questions } = useSelector(({ questionsArray }) => questionsArray);
-  console.log(questions);
-  return (
-    <>
-      <Header />
-      <RenderQuestion />
-    </>
-  );
-};
+const Game = () => (
+  <>
+    <Header />
+    <RenderQuestion />
+  </>
+);
 
 export default Game;
