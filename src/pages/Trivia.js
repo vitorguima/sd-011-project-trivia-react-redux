a0c0805c27589b06911a5afef176895bdc432285
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-07-15 17:15:51 -0300

    main-group-18-requisito4

--- src/pages/Trivia.js
+++ src/pages/Trivia.js
@@ -3,13 +3,11 @@ import { connect } from 'react-redux';
 import Login from './Login';
 
 class Trivia extends React.Component {
-
   render() {
-
     return (
       <header>
         <img data-testid="header-profile-picture" alt="profile picture"/>
-
+        <h2 data-testid="header-player-name">{ localStorage.getItem('state') }</h2>
         <h2 data-testid="header-score">0</h2>
       </header>
     );
