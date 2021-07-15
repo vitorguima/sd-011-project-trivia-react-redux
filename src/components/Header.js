747ac84d94a2c8c3e4a395c9f695d4b9e3281f68
Guilherme Nunes <guiicnunes@gmail.com>
2021-07-15 16:11:22 -0300

    Feat: requisito 4

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+
+class Header extends Component {
+  convertEmail(email) {
+    const hash = md5(email).toString();
+    return `https://www.gravatar.com/avatar/${hash}`;
+  }
+
+  render() {
+    const { player: { name, gravatarEmail, score } } = this.props;
+    return (
+      <>
+        <img
+          src={ this.convertEmail(gravatarEmail) }
+          alt="Avatar do Jogador"
+          data-testid="header-profile-picture"
+        />
+        <div data-testid="header-player-name">{ name }</div>
+        <div data-testid="header-score">{ score }</div>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  player: state.playerReducer,
+});
+
+Header.propTypes = {
+  player: PropTypes.shape({
+    name: PropTypes.string,
+    gravatarEmail: PropTypes.string,
+    score: PropTypes.number,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
