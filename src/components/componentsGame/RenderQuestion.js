7ae9ef4dc00e4fee0071069d544f6cfd6a75e21f
Gustavo Lemes <gustavolemes997@gmail.com>
2021-07-15 16:59:49 -0300

    Requisito 7

--- src/components/componentsGame/RenderQuestion.js
+++ src/components/componentsGame/RenderQuestion.js
@@ -1,38 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { rquestQuestions } from '../../actions';
 
 const RenderQuestion = () => {
   const dispatch = useDispatch();
-  // Random questions https://stackoverflow.com/questions/59329807/react-map-function-return-one-item-at-random
+  const renderOptionsQuetion = () => {
+    const limitedTime = 1000;
+
+    setTimeout(() => {
+      dispatch(rquestQuestions());
+    }, limitedTime);
+  };
+
+  const [state, setState] = useState({ correctAnswer: '', wrongAnswer: '' });
+
   useEffect(() => {
     renderOptionsQuetion();
   }, []);
 
   const { questions } = useSelector(({ questionsArray }) => questionsArray);
 
-  const renderOptionsQuetion = () => {
-    setTimeout(() => {
-      dispatch(rquestQuestions());
-    }, 1000);   
+  const renderLoading = () => <div>Loading...</div>;
+
+  const setClassName = () => {
+    setState({ correctAnswer: 'correct-answer', wrongAnswer: 'wrong-answer' });
   };
 
-  const renderLoading = () => {
-    return <div>Loading...</div>
-  };
-
-  const renderResult = () => {
-    return <div className="question">
-    <span data-testid="question-category">{ questions[0].category }</span>
-    <span data-testid="question-text">{ questions[0].question }</span>
-    <button type="button" data-testid="correct-answer">{ questions[0].correct_answer }</button>
-    { questions[0].incorrect_answers.map((incorrects, index) =>
-    <button type="button" key={ index } data-testid={ `wrong-answer-${index}` }>
-    { incorrects }</button>) }
-  </div>;
-  }
-
-  return ( questions[0] === undefined ? renderLoading() : renderResult());
+  const renderResult = () => (
+    <div className="question">
+      <span data-testid="question-category">{ questions[0].category }</span>
+      <span data-testid="question-text">{ questions[0].question }</span>
+      <button
+        onClick={ () => setClassName() }
+        className={ state.correctAnswer }
+        type="button"
+        data-testid="correct-answer"
+      >
+        { questions[0].correct_answer }
+      </button>
+      { questions[0].incorrect_answers.map((incorrects, index) => (
+        <button
+          onClick={ () => setClassName() }
+          className={ state.wrongAnswer }
+          type="button"
+          key={ index }
+          data-testid={ `wrong-answer-${index}` }
+        >
+          { incorrects }
+        </button>)) }
+    </div>);
+  return (questions[0] === undefined ? renderLoading() : renderResult());
 };
 
 export default RenderQuestion;
