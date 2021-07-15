06aebbe774bcfec4965bf303e0dc25e6472e2b4b
Jean Esteves <jean.esteves2001@gmail.com>
2021-07-15 19:01:28 -0300

    criando redirect para a pag feedbacks

---
+++ src/pages/Feedbacks.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Feedbacks extends Component {
+  render() {
+    return (
+      <div>
+        Feedback
+      </div>
+    );
+  }
+}
