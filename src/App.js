96369f912276ca3678e49947177f4890a502f2b7
Leandro Lopes <leandro_carlos@uol.com.br>
2021-07-15 19:00:20 -0300

    requisitos 2,3,4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
+import GameScreen from './pages/GameScreen';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-        <Login />
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/settings" component={ Settings } />
+        <Route exact path="/game" component={ GameScreen } />
+      </Switch>
     </div>
   );
 }
