0bbb6044dff0fc61c157762c7d8a278884317ee5
Guilherme Nunes <guiicnunes@gmail.com>
2021-07-15 15:44:59 -0300

    Fix: lint requisito 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { login } from '../actions';
 import getToken from '../services/index';
 
