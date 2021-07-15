4e55e3ad6e4627de4d32d5326ad006475bae3d80
marlon307 <marlon-ramosb@hotmail.com>
2021-07-15 17:24:34 -0300

    Rq 8

--- src/components/componentsGame/RenderQuestion.js
+++ src/components/componentsGame/RenderQuestion.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { rquestQuestions } from '../../actions';
+import ComponentTime from './ComponentTime';
 
 const RenderQuestion = () => {
   const dispatch = useDispatch();
   // Random questions https://stackoverflow.com/questions/59329807/react-map-function-return-one-item-at-random
   const { questions } = useSelector(({ questionsArray }) => questionsArray);
+  const [enambe, setEnable] = useState(false);
 
   const renderOptionsQuetion = () => {
     const time = 1000;
@@ -19,16 +21,21 @@ const RenderQuestion = () => {
   }, []);
 
   const renderLoading = () => <div>Loading...</div>;
-
   const renderResult = () => (
     <div className="question">
+      <ComponentTime stateButtonsEnable={ setEnable } />
       <span data-testid="question-category">{ questions[0].category }</span>
       <span data-testid="question-text">{ questions[0].question }</span>
-      <button type="button" data-testid="correct-answer">
+      <button type="button" data-testid="correct-answer" disabled={ enambe }>
         { questions[0].correct_answer }
       </button>
       { questions[0].incorrect_answers.map((incorrects, index) => (
-        <button type="button" key={ index } data-testid={ `wrong-answer-${index}` }>
+        <button
+          type="button"
+          key={ index }
+          data-testid={ `wrong-answer-${index}` }
+          disabled={ enambe }
+        >
           { incorrects }
         </button>)) }
     </div>
