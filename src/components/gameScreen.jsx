d0bdad4af52ec403bb20a36c903aabc5da47bd54
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-07-15 16:32:18 -0300

    Requisito 5 OK, 6 necessario verificar - lint nok

--- src/components/gameScreen.jsx
+++ src/components/gameScreen.jsx
@@ -2,33 +2,20 @@ import React, { Component } from 'react';
 
 export default class gameScreen extends Component {
   render() {
-    console.log(this.props);
-    const category = 'multiple';
+    const { results } = this.props;
     return (
       <div>
-        <span data-testId="question-category">Categoria</span>
-        <h2 data-testId="question-text">Texto da questão</h2>
-        {
-          category === 'multiple'
-            ? (
-              <div>
-                <button type="button" data-testid="{`wrong-answer-{index}`}">q1</button>
-                <button type="button" data-testid="{`wrong-answer-{index}`}">q2</button>
-                <button type="button" data-testid="{`wrong-answer-{index}`}">q3</button>
-                <button type="button" data-testid="correct-answer">q4</button>
-              </div>
-            ) : (
-              <div>
-                <button type="button" data-testid="{`wrong-answer-{index}`}">
-                  verdadeiro
-                </button>
+        {results.map((question, index) => (
+          <div key={index}>
+            <p data-testid="question-category">Categoria: {question.category}</p>
+            <p data-testid="question-text">Pergunta: {question.question}</p>
+            <button data-testid="correct-answer">{question.correct_answer}</button>
+            {question.incorrect_answers.map((incorrectAnswer, index) => (
+              <button data-testid={ `wrong-answer-${index}` } >{incorrectAnswer}</button>
+            ))}
+          </div>
+        ))}
 
-                <button type="button" data-testid="correct-answer">
-                  falso
-                </button>
-              </div>
-            )
-        }
       </div>
     );
   }
