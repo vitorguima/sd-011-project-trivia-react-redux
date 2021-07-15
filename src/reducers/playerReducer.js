0261b0fb88df466158c25d549f181ecdfebd9ab6
Alan Gonçalves <vekio.soft@gmail.com>
2021-07-14 23:19:48 -0300

    add action e reducer - req 4

---
+++ src/reducers/playerReducer.js
@@ -0,0 +1,19 @@
+import { LOGIN, GET_SCORE } from '../actions/gameAction';
+
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+  score: 0,
+};
+
+function playerReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case LOGIN:
+    return { ...state, name: action.name, email: action.email };
+  case GET_SCORE:
+    return { ...state, score: action.score };
+  default:
+    return state;
+  }
+}
+export default playerReducer;
