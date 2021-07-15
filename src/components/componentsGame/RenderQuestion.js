d98072bcbdc47a0801c9094f6ace8fe835a19bb7
marlon307 <marlon-ramosb@hotmail.com>
2021-07-15 17:51:06 -0300

    Rq 8

--- src/components/componentsGame/RenderQuestion.js
+++ src/components/componentsGame/RenderQuestion.js
@@ -2,47 +2,43 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { rquestQuestions } from '../../actions';
 import ComponentTime from './ComponentTime';
+// Random questions https://stackoverflow.com/questions/59329807/react-map-function-return-one-item-at-random
 
 const RenderQuestion = () => {
   const dispatch = useDispatch();
-  // Random questions https://stackoverflow.com/questions/59329807/react-map-function-return-one-item-at-random
   const { questions } = useSelector(({ questionsArray }) => questionsArray);
   const [enambe, setEnable] = useState(false);
+  const [state, setState] = useState({ correctAnswer: '', wrongAnswer: '' });
   const renderOptionsQuetion = () => {
     const limitedTime = 1000;
-
     setTimeout(() => {
       dispatch(rquestQuestions());
     }, limitedTime);
   };
-
-  const [state, setState] = useState({ correctAnswer: '', wrongAnswer: '' });
-
   useEffect(() => {
     renderOptionsQuetion();
   }, []);
-
-  const { questions } = useSelector(({ questionsArray }) => questionsArray);
   const renderLoading = () => <div>Loading...</div>;
-
   const setClassName = () => {
     setState({ correctAnswer: 'correct-answer', wrongAnswer: 'wrong-answer' });
   };
-  
+  const Button = () => (
+    <button
+      onClick={ () => setClassName() }
+      disabled={ enambe }
+      className={ state.correctAnswer }
+      type="button"
+      data-testid="correct-answer"
+    >
+      { questions[0].correct_answer }
+    </button>
+  );
   const renderResult = () => (
     <div className="question">
       <ComponentTime stateButtonsEnable={ setEnable } />
       <span data-testid="question-category">{ questions[0].category }</span>
       <span data-testid="question-text">{ questions[0].question }</span>
-      <button type="button" data-testid="correct-answer" disabled={ enambe }>
-      <button
-        onClick={ () => setClassName() }
-        className={ state.correctAnswer }
-        type="button"
-        data-testid="correct-answer"
-      >
-        { questions[0].correct_answer }
-      </button>
+      { Button() }
       { questions[0].incorrect_answers.map((incorrects, index) => (
         <button
           onClick={ () => setClassName() }
@@ -53,8 +49,7 @@ const RenderQuestion = () => {
           data-testid={ `wrong-answer-${index}` }
         >
           { incorrects }
-        </button>)) 
-      }
+        </button>)) }
     </div>);
   return (questions[0] === undefined ? renderLoading() : renderResult());
 };
