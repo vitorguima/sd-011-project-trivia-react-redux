481b99c61996ec3cfded68a57176024bb0123a83
Diego Ribeiro <dihrm2001@gmail.com>
2021-07-16 15:37:56 -0300

    Requisito 16 feito

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -30,6 +30,14 @@ class Feedback extends Component {
             Jogar novamente
           </button>
         </Link>
+        <Link to="/ranking">
+          <button
+            type="button"
+            data-testid="btn-ranking"
+          >
+            Ver Ranking
+          </button>
+        </Link>
       </div>
     );
   }
---
+++ src/components/Ranking.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Ranking extends Component {
+  // constructor(props) {
+  //   super(props)
+
+  //   this.state = {
+  //   }
+  // }
+
+  render() {
+    return (
+      <h1 data-testid="ranking-title">Ranking</h1>
+    );
+  }
+}
+
+export default Ranking;
