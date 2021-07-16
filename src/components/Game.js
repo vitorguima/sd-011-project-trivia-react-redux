1bfffc23618a9c906b8b6c05632fc13e4f83d204
Amandha W. Barok <amandha.barok@gmail.com>
2021-07-15 21:25:51 -0700

    Finishing the seventh requiriment

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,29 +4,50 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      answered: false,
+    };
+    this.handleAnsweredQuestion = this.handleAnsweredQuestion.bind(this);
+  }
+
+  handleAnsweredQuestion() {
+    this.setState({
+      answered: true,
+    });
+  }
+
   renderQuestions() {
     const { questions: { allQuestions } } = this.props;
+    const { answered } = this.state;
     if (allQuestions.length === 0) {
       return null;
     }
     const currentIndex = 0;
     const currentQuestion = allQuestions[currentIndex];
+    const correctAnswerClassName = answered ? 'correct_answer_btn' : '';
+    const wrongAnswerClassName = answered ? 'wrong_answer_btn' : '';
     return (
       <div>
         <p data-testid="question-category">{currentQuestion.category}</p>
         <p data-testid="question-text">{currentQuestion.question}</p>
         <button
+          className={ correctAnswerClassName }
           type="button"
           data-testid="correct-answer"
+          onClick={ this.handleAnsweredQuestion }
         >
           {currentQuestion.correct_answer}
         </button>
         {
           currentQuestion.incorrect_answers.map((item, index) => (
             <button
+              className={ wrongAnswerClassName }
               type="button"
               key={ index }
               data-testid={ `wrong-answer${index}` }
+              onClick={ this.handleAnsweredQuestion }
             >
               {item}
             </button>
@@ -52,7 +73,9 @@ const mapStateToProps = (state) => ({
 });
 
 Game.propTypes = {
-  questions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  questions: PropTypes.shape({
+    allQuestions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  }).isRequired,
 };
 
 export default connect(mapStateToProps, null)(Game);
