ee4200cc782fd243fb4834e54e369aabffbd0180
Alberto Candido <albertocandido98@gmail.com>
2021-07-16 17:59:11 -0300

    Finaliza requisito bonus

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,25 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { changeSettings } from '../redux/actions';
-// amount=9 - número de perguntas
-
-// categorias
-// &category=10 - books
-// &category=27 - animals
-// &category=18 - computers
-// &category=19 - mathematics
-// &category=29 - comics
-// &category=11 - movies
-
-// dificuldade
-// &difficulty=easy
-// &difficulty=medium
-// &difficulty=hard
-
-// tipo de perguntas
-// &type=multiple
-// &type=boolean
 
 class Settings extends Component {
   constructor(props) {
@@ -29,6 +12,7 @@ class Settings extends Component {
       type: '',
       questionsNumber: 5,
       difficulty: '',
+      shouldRedirect: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -61,6 +45,7 @@ class Settings extends Component {
     if (difficulty) url += difficulty;
     url += '&token=';
     userSettings(url);
+    this.setState({ shouldRedirect: true });
   }
 
   renderOptionsCategory(categories) {
@@ -71,53 +56,61 @@ class Settings extends Component {
     );
   }
 
-  render() {
+  renderForm() {
     const { questionsNumber, type, category, difficulty } = this.state;
     const categories = this.categoriesList();
     const handler = this.handleChange;
     return (
+      <form>
+        <label htmlFor="questionsNumber">
+          Selecione o número de perguntas do jogo
+          <input
+            type="number"
+            name="questionsNumber"
+            value={ questionsNumber }
+            onChange={ this.handleChange }
+            min="5"
+            max="20"
+          />
+        </label>
+        <label htmlFor="category">
+          Categoria
+          <select id="category" onChange={ handler } name="category" value={ category }>
+            {this.renderOptionsCategory(categories)}
+          </select>
+        </label>
+        <label htmlFor="difficulty">
+          Dificuldade
+          <select
+            onChange={ handler }
+            id="difficulty"
+            name="difficulty"
+            value={ difficulty }
+          >
+            <option namRedirectme="difficulty" value="&difficulty=easy">Easy</option>
+            <option namRedirectme="difficulty" value="&difficulty=medium">Medium</option>
+            <option namRedirectme="difficulty" value="&difficulty=hard">Hard</option>
+          </select>
+        </label>
+        <label htmlFor="type">
+          Tipo de perguntas
+          <select onChange={ handler } id="type" name="type" value={ type }>
+            <option name="type" value="&type=multiple">Multiple Choice</option>
+            <option name="type" value="&type=boolean">True / False</option>
+          </select>
+        </label>
+        <button onClick={ this.handleClick } type="button">Salvar configurações</button>
+      </form>
+    );
+  }
+
+  render() {
+    const { shouldRedirect } = this.state;
+    if (shouldRedirect) return <Redirect to="/" />;
+    return (
       <div>
         <title data-testid="settings-title">Settings</title>
-        <form>
-          <label htmlFor="questionsNumber">
-            Selecione o número de perguntas do jogo
-            <input
-              type="number"
-              name="questionsNumber"
-              value={ questionsNumber }
-              onChange={ this.handleChange }
-              min="5"
-              max="20"
-            />
-          </label>
-          <label htmlFor="category">
-            Categoria
-            <select id="category" onChange={ handler } name="category" value={ category }>
-              {this.renderOptionsCategory(categories)}
-            </select>
-          </label>
-          <label htmlFor="difficulty">
-            Dificuldade
-            <select
-              onChange={ handler }
-              id="difficulty"
-              name="difficulty"
-              value={ difficulty }
-            >
-              <option name="difficulty" value="&difficulty=easy">Easy</option>
-              <option name="difficulty" value="&difficulty=medium">Medium</option>
-              <option name="difficulty" value="&difficulty=hard">Hard</option>
-            </select>
-          </label>
-          <label htmlFor="type">
-            Tipo de perguntas
-            <select onChange={ handler } id="type" name="type" value={ type }>
-              <option name="type" value="&type=multiple">Multiple Choice</option>
-              <option name="type" value="&type=boolean">True / False</option>
-            </select>
-          </label>
-          <button onClick={ this.handleClick } type="button">Salvar configurações</button>
-        </form>
+        {this.renderForm()}
       </div>
     );
   }
