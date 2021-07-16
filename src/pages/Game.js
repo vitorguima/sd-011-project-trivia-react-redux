9f3945a5dc0525fa11a2f8bebf4f2794292d9aca
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-16 17:47:15 -0300

    requisito 10 concluido

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { getQuestionsThunk } from '../actions';
 import './Game.css';
 
@@ -8,6 +9,7 @@ class Game extends Component {
   constructor() {
     super();
     this.state = {
+      number: 0,
       disabled: false,
       seconds: 30,
     };
@@ -15,16 +17,17 @@ class Game extends Component {
     this.colorOptions = this.colorOptions.bind(this);
     this.startTimer = this.startTimer.bind(this);
     this.timer = this.timer.bind(this);
+    this.handleQuestion = this.handleQuestion.bind(this);
   }
 
   componentDidMount() {
     const { getQuestions } = this.props;
-    const seconds = 1000;
+    const miliSeconds = 1000;
     if (localStorage.token) {
       getQuestions(localStorage.getItem('token'));
     }
     this.startTimer();
-    this.interval = setInterval(this.timer, seconds);
+    this.interval = setInterval(this.timer, miliSeconds);
   }
 
   colorOptions() {
@@ -34,8 +37,10 @@ class Game extends Component {
     wrongAnswers.forEach((answer) => {
       answer.classList.add('wrong-color');
     });
+    clearInterval(this.interval);
     this.setState({
       disabled: true,
+      seconds: 0,
     });
   }
 
@@ -59,9 +64,66 @@ class Game extends Component {
     }
   }
 
+  handleQuestion() {
+    const miliSeconds = 1000;
+    const rightAnswer = document.querySelector('#correct-answer');
+    const wrongAnswers = document.querySelectorAll('#wrong-answer');
+    rightAnswer.classList = '';
+    wrongAnswers.forEach((answer) => {
+      answer.classList = '';
+    });
+    this.setState((state) => ({
+      number: state.number + 1,
+      disabled: false,
+      seconds: 30,
+    }));
+    this.startTimer();
+    this.interval = setInterval(this.timer, miliSeconds);
+  }
+
+  renderQuestions() {
+    const { questions } = this.props;
+    const { disabled, number } = this.state;
+    return (
+      <div>
+        <h5
+          data-testid="question-category"
+        >
+          {questions.results[number].category}
+        </h5>
+        <h2 data-testid="question-text">{questions.results[number].question}</h2>
+        <button
+          type="button"
+          data-testid="correct-answer"
+          id="correct-answer"
+          onClick={ this.colorOptions }
+          disabled={ disabled }
+        >
+          {questions.results[number].correct_answer}
+        </button>
+        {questions.results[number].incorrect_answers.map((incorrect, key) => (
+          <button
+            key={ key }
+            type="button"
+            data-testid={ `wrong-answer ${key}` }
+            id="wrong-answer"
+            onClick={ this.colorOptions }
+            disabled={ disabled }
+          >
+            { incorrect }
+          </button>
+        ))}
+      </div>
+    );
+  }
+
   render() {
-    const { loading, hash, name, score, questions } = this.props;
-    const { disabled, seconds } = this.state;
+    const { loading, hash, name, score } = this.props;
+    const { disabled, seconds, number } = this.state;
+    const maxQuestions = 5;
+    if (number === maxQuestions) {
+      return <Redirect to="/" />;
+    }
     return (
       <div>
         <header>
@@ -72,34 +134,19 @@ class Game extends Component {
         <div>
           <p id="timer">{ seconds }</p>
           {!loading
-            ? (
-              <div>
-                <h5 data-testid="question-category">{questions.results[0].category}</h5>
-                <h2 data-testid="question-text">{questions.results[0].question}</h2>
-                <button
-                  type="button"
-                  data-testid="correct-answer"
-                  id="correct-answer"
-                  onClick={ this.colorOptions }
-                  disabled={ disabled }
-                >
-                  {questions.results[0].correct_answer}
-                </button>
-                {questions.results[0].incorrect_answers.map((incorrect, key) => (
-                  <button
-                    key={ key }
-                    type="button"
-                    data-testid={ `wrong-answer ${key}` }
-                    id="wrong-answer"
-                    onClick={ this.colorOptions }
-                    disabled={ disabled }
-                  >
-                    { incorrect }
-                  </button>
-                ))}
-              </div>) : <p>loading...</p>}
+            ? (this.renderQuestions()) : <p>loading...</p>}
           {disabled
-            ? <button data-testid="btn-next" type="button">Próxima</button> : null}
+            ? (
+              <button
+                onClick={ this.handleQuestion }
+                data-testid="btn-next"
+                type="button"
+                id="btnNext"
+              >
+                Próxima
+              </button>
+            )
+            : null}
         </div>
       </div>
     );
