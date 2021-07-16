f3b76b7217eb6eba5a3d80e83329210734aeabfe
Marco Galindo <marco1408@gmail.com>
2021-07-16 15:44:05 -0300

    requisito 12 finalizado

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      score: 0,
+    };
+    this.getFeedback = this.getFeedback.bind(this);
+  }
+
+  componentDidMount() {
+    this.getFeedback();
+  }
+
+  getFeedback() {
+    const state = JSON.parse(localStorage.getItem('state'));
+    this.setState({
+      name: state.player.name,
+      score: state.player.score,
+    });
+  }
+
   render() {
+    const { name, score } = this.state;
+    const { img } = this.props;
     return (
-      <div>
-        <p data-testid="feedback-text">Feedback</p>
-      </div>
+      <header>
+        <h1 data-testid="feedback-text">Feedback</h1>
+        <p data-testid="header-player-name">{name}</p>
+        <img data-testid="header-profile-picture" src={ img } alt="Player avatar" />
+        <p data-testid="header-score">{score}</p>
+      </header>
     );
   }
 }
+
+const MapStateToProps = (state) => ({
+  img: state.login.img,
+});
+
+Feedback.propTypes = {
+  img: PropTypes.string,
+}.isRequired;
+
+export default connect(MapStateToProps)(Feedback);
