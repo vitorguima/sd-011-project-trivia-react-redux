7ae328342f33e69dc842ab6a0ab60349a0962460
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-07-16 15:03:27 -0300

    Começando pagina feedback

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 'feedback' );
+  }
+}
+ 
+export default Feedback;
