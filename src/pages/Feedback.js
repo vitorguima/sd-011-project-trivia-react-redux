1329dcbc16c43d79a86e19d4a9f1b03b292c8626
Diego Figueirêdo <8kball@gmail.com>
2021-07-16 14:34:31 -0300

    Requisito 4

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -13,7 +13,8 @@ export default class FeedBack extends Component {
   }
 
   render() {
-    const { player: { name, score, gravatarEmail } } = this.getPlayerFromLocalStorage();
+    const { player } = this.getPlayerFromLocalStorage();
+    const { name, score, gravatarEmail, assertions } = player;
     const gravatarHash = md5(gravatarEmail.trim().toLowerCase()).toString();
     const gravatarPicture = `https://www.gravatar.com/avatar/${gravatarHash}`;
     return (
@@ -22,6 +23,8 @@ export default class FeedBack extends Component {
         <img data-testid="header-profile-picture" src={ gravatarPicture } alt="Player" />
         <h2 data-testid="header-player-name">{ name }</h2>
         <h3 data-testid="header-score">{ score }</h3>
+        <h4 data-testid="feedback-total-score">{ score }</h4>
+        <h4 data-testid="feedback-total-question">{ assertions }</h4>
       </div>
     );
   }
