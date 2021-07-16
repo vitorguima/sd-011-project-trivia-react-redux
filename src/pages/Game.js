599e82bfb2503bea8d1bb06a3518fba22a4986d1
josanjohnata <josanjohnata@gmail.com>
2021-07-16 18:05:04 -0300

    Adiciona requisito 7

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
-
+// nana
 class Game extends Component {
   constructor() {
     super();
