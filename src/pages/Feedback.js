4be32d853bd1429569d44b6329addca24e753195
Diego Figueirêdo <8kball@gmail.com>
2021-07-16 14:37:00 -0300

    Remoneia Feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react';
-import md5 from 'crypto-js/md5';
 
-export default class FeedBack extends Component {
-  constructor() {
-    super();
-    this.getPlayerFromLocalStorage = this.getPlayerFromLocalStorage.bind(this);
-  }
+// ******PÁGINA FAKE, APENAS PARA APROVAR O FINAL DO REQUISITO 11, MANTENHAM A PÁGINA DE VOCÊS*******
 
-  getPlayerFromLocalStorage() {
-    const playerInfo = localStorage.getItem('state');
-    return JSON.parse(playerInfo);
-  }
-
+class FeedBack extends Component {
   render() {
-    const { player } = this.getPlayerFromLocalStorage();
-    const { name, score, gravatarEmail, assertions } = player;
-    const gravatarHash = md5(gravatarEmail.trim().toLowerCase()).toString();
-    const gravatarPicture = `https://www.gravatar.com/avatar/${gravatarHash}`;
     return (
-      <div className="feedback__container">
-        <h1 data-testid="feedback-text">FeedBack</h1>
-        <img data-testid="header-profile-picture" src={ gravatarPicture } alt="Player" />
-        <h2 data-testid="header-player-name">{ name }</h2>
-        <h3 data-testid="header-score">{ score }</h3>
-        <h4 data-testid="feedback-total-score">{ score }</h4>
-        <h4 data-testid="feedback-total-question">{ assertions }</h4>
-      </div>
+      <h1 data-testid="feedback-text">FeedBack</h1>
     );
   }
 }
+
+export default FeedBack;
