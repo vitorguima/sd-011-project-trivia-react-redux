8dfd297b18273cfd686a9c0f9b819899fc0d78f2
Gustavo Mendes <gusm217@gmail.com>
2021-07-16 15:45:25 -0300

    Corrige importaçao header

--- src/pages/GamePage/GamePage.js
+++ src/pages/GamePage/GamePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Questions from '../../components/Questions';
+import Header from '../../components/Header';
 
 class GamePage extends Component {
   render() {
