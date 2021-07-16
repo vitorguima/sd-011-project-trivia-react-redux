17a2333a3008e60af0fff25c52dcecf4aff08e83
marsleite <marsleite@gmail.com>
2021-07-16 13:04:14 -0300

    req 13 done

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+
+    this.msg = this.msg.bind(this);
+  }
+
+  msg() {
+    const three = 3;
+    const { assertions } = this.props; // quantidade de acertos que vem do estado do redux
+    const message = assertions < three ? 'Podia ser melhor...' : 'Mandou bem!';
+    return message;
+  }
+
   render() {
     return (
       <div>
         <Header />
+        <h2 data-testid="feedback-text">{ this.msg }</h2>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
 
 export default Feedback;
