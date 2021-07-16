6beccf7772621749848e99ccfdab88ec128b928f
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-07-16 17:38:54 -0300

    Requisito 9 OK - Lint OK

--- src/components/gameScreen.jsx
+++ src/components/gameScreen.jsx
@@ -4,13 +4,44 @@ import PropTypes from 'prop-types';
 export default class gameScreen extends Component {
   constructor() {
     super();
-
+    const { player } = JSON.parse(localStorage.getItem('state'));
     this.state = {
       question: 0,
+      player,
     };
 
     this.colorSelectCorrect = this.colorSelectCorrect.bind(this);
     this.nextQuestion = this.nextQuestion.bind(this);
+    this.correctAnswer = this.correctAnswer.bind(this);
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
+
+  onClickHandler() {
+    this.colorSelectCorrect();
+    this.correctAnswer();
+  }
+
+  correctAnswer() {
+    const fixedPoints = 10;
+    const { question, player } = this.state;
+    const { results } = this.props;
+    let difValue = 0;
+    const hard = 3;
+    const medium = 2;
+    const easy = 1;
+    const cronoValue = document.querySelector('.cronometer').innerHTML.split(':')[1];
+    if ((results[question].difficulty) === 'hard') {
+      difValue = hard;
+    } if ((results[question].difficulty) === 'medium') {
+      difValue = medium;
+    } if ((results[question].difficulty) === 'easy') {
+      difValue = easy;
+    }
+    localStorage.setItem('state', JSON.stringify({ player:
+      { name: player.name,
+        assertions: player.assertions + 1,
+        score: player.score + (fixedPoints + (cronoValue * difValue)),
+        gravatarEmail: player.email } }));
   }
 
   colorSelectCorrect() {
@@ -41,7 +72,7 @@ export default class gameScreen extends Component {
               type="button"
               data-testid="correct-answer"
               className="green-border"
-              onClick={ () => this.colorSelectCorrect() }
+              onClick={ () => this.onClickHandler() }
             >
               {results[question].correct_answer}
             </button>
