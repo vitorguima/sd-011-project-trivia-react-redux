9a9dab9c0b099248712f403974e309c09be27174
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-07-16 18:09:25 -0300

    requisito 13 completo

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,13 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Feedback() {
-  useEffect(() => {
-    console.log('teste');
-  }, []);
+  const correctAnswers = useSelector((state) => state.player.state);
+  const { assertions, score } = correctAnswers;
+  const parameterNumber = 3;
+
+  const showFeedbackMessage = (hits) => {
+    if (hits < parameterNumber) return 'Podia ser melhor...';
+    if (hits >= parameterNumber) return 'Mandou bem!';
+  };
 
   return (
-    <div data-testid="feedback-text">
-      FEEDBACK PAGE
+    <div>
+      <p data-testid="feedback-text">{showFeedbackMessage(assertions)}</p>
+      <p data-testid="feedback-total-score">{`Score final: ${score}`}</p>
+      <p data-testid="feedback-total-question">{`Acertou: ${assertions} perguntas`}</p>
     </div>
   );
 }
