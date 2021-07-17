8bd9727484176c93916b6e84a0e237e2f8d8c19c
Thyago Pessoa <flash_thyago@hotmail.com>
2021-07-17 20:10:54 -0300

    Requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Ranking extends Component {
   render() {
     return (
       <div>
         <h4 data-testid="ranking-title">Página de Ranking</h4>
+        <Link exact to="/">
+          <button
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Início
+          </button>
+        </Link>
       </div>
     );
   }
