f8462f155718176901fb3d0667a52fb104cb7d00
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-07-17 12:14:52 -0300

    Requisito 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import md5 from 'crypto-js/md5';
 
 export default class Feedback extends Component {
+  handleGravatar() {
+    const localStg = JSON.parse(localStorage.getItem('state'));
+    const { gravatarEmail } = localStg.player;
+    return md5(gravatarEmail).toString();
+  }
+
   render() {
+    const localStg = JSON.parse(localStorage.getItem('state'));
+    const { score, name } = localStg.player;
     return (
-      <h1 data-testid="feedback-text">
-        Fd
-      </h1>
+      <header>
+        <img
+          data-testid="header-profile-picture"
+          src={ `https://www.gravatar.com/avatar/${this.handleGravatar()}` }
+          alt="Gravatar"
+        />
+        <h2 data-testid="header-player-name">
+          Usuário:
+          { name }
+        </h2>
+        <p data-testid="header-score">{ score }</p>
+        <h1 data-testid="feedback-text">
+          Fd
+        </h1>
+      </header>
+
     );
   }
 }
