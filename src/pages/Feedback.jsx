6c8617bda2cc2f6fda29e8c53b2ab6febb54f7fb
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-07-17 11:43:29 -0300

    Requisito 11

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Feedback extends Component {
+  render() {
+    return (
+      <h1 data-testid="feedback-text">
+        Fd
+      </h1>
+    );
+  }
+}
