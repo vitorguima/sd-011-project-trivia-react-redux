d35fcfa394f6c86e66a7aec53727301e0818bdbe
Thyago Pessoa <flash_thyago@hotmail.com>
2021-07-17 07:19:58 -0300

    Requisito 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Feedback extends Component {
   render() {
     return (
       <div>
         <p data-testid="feedback-text">Página de feedback</p>
+        <Header />
       </div>
     );
   }
