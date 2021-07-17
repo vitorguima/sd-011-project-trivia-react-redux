a032351cf3762aad1f27ade090ce7f06428bc005
Thyago Pessoa <flash_thyago@hotmail.com>
2021-07-17 19:15:35 -0300

    Requisito 16

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -48,6 +48,14 @@ export default class Feedback extends Component {
             Jogar novamente
           </button>
         </Link>
+        <Link to="/ranking">
+          <button
+            type="button"
+            data-testid="btn-ranking"
+          >
+            Ver Ranking
+          </button>
+        </Link>
       </div>
     );
   }
