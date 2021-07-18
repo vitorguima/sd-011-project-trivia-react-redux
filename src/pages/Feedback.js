578ca1b5f96c51ee0b1f42283f98720bfddf4d1c
marsleite <marsleite@gmail.com>
2021-07-18 15:05:05 -0300

    req 12 - done

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Feedback extends Component {
   render() {
     return (
       <div>
         <h3 data-testid="feedback-text">Feedback</h3>
+        <Header />
       </div>
     );
   }
