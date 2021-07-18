26d123af827ea6300bbba5890c9506d7fd16cd81
Juliana Happatsch <jhappatsch@gmail.com>
2021-07-18 13:33:32 -0300

    requisito 15 finalizado

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+
+    this.renderLoginPage = this.renderLoginPage.bind(this);
+  }
+
+  renderLoginPage() {
+    const { history } = this.props;
+
+    history.push('/');
+  }
+
   render() {
     return (
-      <div data-testid="feedback-text">
-        Tela de Feedback
+      <div>
+        <div data-testid="feedback-text">
+          Tela de Feedback
+        </div>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.renderLoginPage }
+        >
+          Jogar novamente
+        </button>
       </div>
     );
   }
 }
 
 export default Feedback;
+
+Feedback.propTypes = {
+  history: PropTypes.shape({
+    location: PropTypes.objectOf(PropTypes.string),
+    push: PropTypes.func.isRequired,
+  }),
+}.isRequired;
