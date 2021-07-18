fe8da866fae9db57d88ed2670ac6cb76a3cb1a4f
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-07-18 14:10:03 -0300

    Realizado requisito 14 no componente performance, filho de feedback. Testes de 1 a 10 passando. 11 com o erro que empede demais testes, porem requisito passou no teste de url.

--- src/components/Performance.js
+++ src/components/Performance.js
@@ -12,27 +12,32 @@ class Performance extends Component {
     const msg1 = 'Podia ser melhor...';
     const msg2 = 'Mandou bem!';
     const expectedPerformace = 3;
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
 
     return (
-      <p
-        data-testid="feedback-text"
-      >
-        {
-          (assertions >= expectedPerformace)
-            ? msg2 : msg1
-        }
-      </p>
+      <div>
+        <p data-testid="feedback-text">
+          {(assertions >= expectedPerformace) ? msg2 : msg1}
+        </p>
+        <p data-testid="feedback-total-question">
+          {`Você acertou ${assertions} questões!`}
+        </p>
+        <p data-testid="feedback-total-score">
+          {`Um total de  ${score} pontos`}
+        </p>
+      </div>
     );
   }
 }
 
 Performance.propTypes = {
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   assertions: state.triviaReducer.assertions,
+  score: state.triviaReducer.score,
 });
 
 export default connect(mapStateToProps)(Performance);
