ff87a79dc781fb72b44e2cb4f151f313e7272b72
Marco Galindo <marco1408@gmail.com>
2021-07-19 15:53:08 -0300

    requisito 17 em andamento

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,9 +1,70 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Ranking extends Component {
+class Ranking extends Component {
+  constructor() {
+    super();
+    this.state = {
+      players: [],
+    };
+    this.getRankings = this.getRankings.bind(this);
+    this.setRankings = this.setRankings.bind(this);
+  }
+
+  componentDidMount() {
+    this.setRankings();
+  }
+
+  setRankings() {
+    const { name, img, score } = this.props;
+    let ranking = [];
+    if (localStorage.ranking) {
+      ranking = JSON.parse(localStorage.getItem('ranking'));
+    }
+    const player = {
+      name,
+      score,
+      picture: img,
+    };
+    ranking.push(player);
+    localStorage.setItem('ranking', JSON.stringify(ranking));
+    this.getRankings();
+  }
+
+  getRankings() {
+    let players = [];
+    players = JSON.parse(localStorage.getItem('ranking'));
+    this.setState({ players });
+  }
+
   render() {
+    const { players } = this.state;
     return (
-      <h1 data-testid="ranking-title"> Ranking </h1>
+      <div>
+        <h1 data-testid="ranking-title"> Ranking </h1>
+        { players.sort((player1, player2) => player2.score - player1.score)
+          .map((player, index) => (
+            <div key={ index }>
+              <img src={ player.picture } alt={ player.name } />
+              <p data-testid={ `player-name-${index}` }>{player.name}</p>
+              <p data-testid={ `player-score-${index}` }>{player.score}</p>
+            </div>)) }
+      </div>
     );
   }
 }
+
+const MapStateToProps = (state) => ({
+  name: state.login.name,
+  img: state.login.img,
+  score: state.game.score,
+});
+
+Ranking.propTypes = {
+  name: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+export default connect(MapStateToProps)(Ranking);
