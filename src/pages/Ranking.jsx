84086adfbac1fbb7582923afe92d1bfc9976b753
Víctor Faria <vitilevi@hotmail.com>
2021-07-19 15:13:23 -0300

    Feito requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Ranking extends Component {
   render() {
@@ -7,6 +8,7 @@ export default class Ranking extends Component {
         <h1 data-testid="ranking-title">
           Ranking
         </h1>
+        <Link to="/" data-testid="btn-go-home">Voltar para a tela inical</Link>
       </div>
     );
   }
