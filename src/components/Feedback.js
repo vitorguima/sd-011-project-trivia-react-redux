3ef3aaf6df3eae35bf3ad5ce0aa04836a84d3199
Rodolfo <rodolforrcont@gmail.com>
2021-07-19 18:31:01 -0300

    Requisito 13

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import md5 from 'crypto-js/md5';
 
 export default class Feedback extends Component {
+  constructor() {
+    super();
+    this.verifyAssetions = this.verifyAssetions.bind(this);
+    // this.seeRanking = this.seeRanking.bind(this);
+  }
+
+  verifyAssetions() {
+    const getLocalStorage = JSON.parse(localStorage.getItem('state'));
+    const magicNum = 3;
+    if (getLocalStorage.player.assertions >= magicNum) {
+      return (
+        <>
+          <p data-testid="feedback-text">Mandou bem!</p>
+          <p data-testid="feedback-total-score">{getLocalStorage.player.score}</p>
+          <p data-testid="feedback-total-question">{getLocalStorage.player.assertions}</p>
+        </>
+      );
+    } return (
+      <>
+        <p data-testid="feedback-text">Podia ser melhor...</p>
+        <p data-testid="feedback-total-score">{getLocalStorage.player.score}</p>
+        <p data-testid="feedback-total-question">{getLocalStorage.player.assertions}</p>
+      </>
+    );
+  }
+
   render() {
     const getLocalStorage = JSON.parse(localStorage.getItem('state'));
     const hashEmail = md5(getLocalStorage.gravatarEmail).toString();
@@ -22,6 +48,7 @@ export default class Feedback extends Component {
               { getLocalStorage.player.score }
             </span>
           </div>
+          { this.verifyAssetions() }
         </header>
       </div>
     );
