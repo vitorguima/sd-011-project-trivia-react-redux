f4a457bf237b4a4dddd0c63bdc6e4a44cf72b283
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-07-19 15:18:49 -0300

    Req 13 realizado

--- src/pages/feedback.jsx
+++ src/pages/feedback.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from './header';
 
-export default class feedback extends Component {
+class Feedback extends Component {
   render() {
+    const { assertions } = this.props;
+    const rightAnswers = 3;
     return (
       <div>
         <h1>Feedback</h1>
         <Header />
-        <p data-testid="feedback-text" />
+        <p data-testid="feedback-text">
+          {assertions < rightAnswers ? (
+            <span>Podia ser melhor...</span>
+          ) : (
+            <span>Mandou bem!</span>
+          )}
+        </p>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  assertions: state.playerReducer.assertions,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Feedback);
