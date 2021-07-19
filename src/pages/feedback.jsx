34a1a8ffdf163913da6b81361c46bf58d0a70e28
Vinicius <viniciusfod@gmail.com>
2021-07-19 15:27:08 -0300

    main-group-42-req15

--- src/pages/feedback.jsx
+++ src/pages/feedback.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from './header';
 
 export default class feedback extends Component {
@@ -8,6 +9,9 @@ export default class feedback extends Component {
         <h1>Feedback</h1>
         <Header />
         <p data-testid="feedback-text" />
+        <Link to="/">
+          <button type="button" data-testid="btn-play-again">Jogar Novamente</button>
+        </Link>
       </div>
     );
   }
