10cd1084d9c5c44f0cf5da3872d504bfe773bf53
Nick-713 <pereiranicolas713@gmail.com>
2021-07-19 17:35:14 -0300

    req14

--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -392,7 +392,7 @@ describe('12 - [TELA DE FEEDBACK] Desenvolva o header de _feedback_ que deve con
   });
 });
 
-describe.only('13 - [TELA DE FEEDBACK] Crie a mensagem de _feedback_ para ser exibida a pessoa usuária', () => {
+describe('13 - [TELA DE FEEDBACK] Crie a mensagem de _feedback_ para ser exibida a pessoa usuária', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
     cy.clearLocalStorage();
@@ -444,7 +444,7 @@ describe.only('13 - [TELA DE FEEDBACK] Crie a mensagem de _feedback_ para ser ex
   });
 });
 
-describe('14 - [TELA DE FEEDBACK] Exiba as informações relacionadas aos resultados obtidos para a pessoa usuária', () => {
+describe.only('14 - [TELA DE FEEDBACK] Exiba as informações relacionadas aos resultados obtidos para a pessoa usuária', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
     cy.clearLocalStorage();
