a2eb7818f15332b025599d2edc8bac8a7eba2c25
Ghislaine <glenox79@gmail.com>
2021-07-19 08:18:18 -0300

    Requisito 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
     return (
-      <div data-testid="feedback-text">
-        Tela de Feedback
+      <div>
+        <Header />
       </div>
+
     );
   }
 }
