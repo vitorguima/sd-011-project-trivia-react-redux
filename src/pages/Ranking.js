596410f03e2b56679fb255d16d3bbd01dc1e376a
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-19 13:21:22 -0300

    requisito 18 resolvido

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -5,6 +5,7 @@ export default class Ranking extends Component {
     return (
       <main>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button data-testid="btn-go-home" type="button">Início</button>
       </main>
     );
   }
