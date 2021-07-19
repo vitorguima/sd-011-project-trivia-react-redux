7a389b167fb6e6a1aba44a74bbe38b8bf62c7351
Vitor <vitorguima@gmail.com>
2021-07-19 20:27:07 -0300

    requisito 17 finalizado

--- src/helpers/styleAnswers.js
+++ src/helpers/styleAnswers.js
@@ -1,4 +1,6 @@
-export default function handleStyleAnswers() {
+import md5 from 'crypto-js/md5';
+
+export function handleStyleAnswers() {
   const styleAnswers = document.getElementsByName('answer');
   styleAnswers.forEach((answerBtn) => {
     if (answerBtn.getAttribute('data-testid') === 'correct-answer') {
@@ -7,4 +9,17 @@ export default function handleStyleAnswers() {
       answerBtn.style = 'border: 3px solid rgb(255, 0, 0)';
     }
   });
+}
+
+export function setRankToStorage(name, email, score) {
+  const picture = `https://www.gravatar.com/avatar/${md5(email).toString()}`;
+
+  if (JSON.parse(localStorage.getItem('ranking'))) {
+    const currentRank = JSON.parse(localStorage.getItem('ranking'));
+    const newRank = [...currentRank, { name, score, picture }];
+    localStorage.setItem('ranking', JSON.stringify(newRank));
+  } else {
+    const newRank = [{ name, score, picture }];
+    localStorage.setItem('ranking', JSON.stringify(newRank));
+  }
 }
