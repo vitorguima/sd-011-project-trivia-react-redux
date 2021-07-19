d3355e93740911f24ae7fa296d286ea2884be275
Juliana Happatsch <jhappatsch@gmail.com>
2021-07-19 16:47:57 -0300

    requisito 18 finalizado

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
+  constructor() {
+    super();
+
+    this.renderLoginPage = this.renderLoginPage.bind(this);
+  }
+
+  renderLoginPage() {
+    const { history } = this.props;
+
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.renderLoginPage }
+        >
+          Voltar ao início
+        </button>
       </div>
     );
   }
 }
 
 export default Ranking;
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    location: PropTypes.objectOf(PropTypes.string),
+    push: PropTypes.func.isRequired,
+  }),
+}.isRequired;
