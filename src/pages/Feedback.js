d0ebca0a1cba4a3fd396ae342eb9c9249d0cc574
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-07-20 16:13:13 -0300

    Adiciona tela de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
+  renderFeedbackMessage() {
+    const { score } = this.props;
+    console.log(score);
+    const number = 3;
+    if (score < number) {
+      return (
+        <h3 data-testid="feedback-text">Podia ser melhor...</h3>
+      );
+    } return <h3 data-testid="feedback-text">Mandou bem!</h3>;
+  }
+
   render() {
     return (
-      <div data-testid="feedback-text">
+      <div>
         <Header />
+        <section>
+          { this.renderFeedbackMessage() }
+        </section>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  score: state.user.score,
+});
+
+Feedback.propTypes = {
+  score: PropTypes.number,
+}.isrequired;
+
+export default connect(mapStateToProps)(Feedback);
