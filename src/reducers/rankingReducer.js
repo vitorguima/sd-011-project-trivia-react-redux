2e4fefe4f5924879fd497a44ec2223f7f044bfc7
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-07-19 21:24:38 -0300

    fix: criacao de nova action para ranking

--- src/reducers/rankingReducer.js
+++ src/reducers/rankingReducer.js
@@ -3,7 +3,7 @@ import { SEND_RANKING } from '../actions';
 INITIAL_STATE = [];
 
 function boardRanking(state = INITIAL_STATE, { type, payload }) {
-  localStorage.setItem('ranking', JSON.stringify(...state, ...payload));
+  localStorage.setItem('ranking', JSON.stringify({ ...state, ...payload }));
 
   switch (type) {
   case SEND_RANKING:
