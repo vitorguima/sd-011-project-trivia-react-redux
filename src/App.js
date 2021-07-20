175a10fd8002bedbb87cbfaae1b6d3d86a95219d
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-07-20 18:54:41 -0300

    conflitos

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Trivia from './pages/Trivia';
+import Feedback from './pages/Feedback';
 import Ranking from './pages/Ranking';
 import './App.css';
-import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
@@ -13,8 +13,8 @@ export default function App() {
       <Route exact path="/" component={ Login } />
       <Route path="/settings" component={ Settings } />
       <Route path="/trivia" component={ Trivia } />
-      <Route path="/ranking" component={ Ranking } />
       <Route path="/feedback" component={ Feedback } />
+      <Route path="/ranking" component={ Ranking } />
     </Switch>
   );
 }
