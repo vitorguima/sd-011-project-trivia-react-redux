39f2b890127e35dac1b964a9771c5f60a3e4131d
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-07-19 22:48:15 -0300

    add req 17 e 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  getRankingPlayers() {
+    return JSON.parse(localStorage.getItem('ranking'));
+  }
+
   render() {
+    const { history } = this.props;
     return (
-      <h1 data-testid="ranking-title">
-        Ranking
-      </h1>
+      <>
+        <h1 data-testid="ranking-title">
+          Ranking:
+        </h1>
+        <ol>
+          {
+            this.getRankingPlayers().map((player, index) => (
+              <li key={ index }>
+                <img
+                  src={ player.picture }
+                  alt="gravatar"
+                />
+                <p data-testid={ `player-name-${index}` }>{player.name}</p>
+                <p data-testid={ `player-score-${index}` }>{player.score}</p>
+              </li>
+            ))
+          }
+        </ol>
+        <button
+          type="button"
+          onClick={ () => history.push('/') }
+          data-testid="btn-go-home"
+        >
+          Início
+        </button>
+      </>
     );
   }
 }
 
 export default Ranking;
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
