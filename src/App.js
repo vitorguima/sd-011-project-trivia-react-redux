c464fbc869023afe7ec556f771b560418ab5b352
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-07-19 22:13:17 -0300

    feat: rota ranking - req 16

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 // import logo from './trivia.png';
 // import './App.css';
 
 export default function App() {
   return (
     <Switch>
+      <Route path="/ranking" component={ Ranking } />
       <Route path="/settings" component={ Settings } />
       <Route path="/feedback" component={ Feedback } />
       <Route path="/game" component={ Game } />
