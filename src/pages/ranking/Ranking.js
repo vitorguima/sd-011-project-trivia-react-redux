401eabd2781821881cae2b6af1b0a5b39b6e6170
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-07-20 03:32:53 -0300

    style pagina de ranking

--- src/pages/ranking/Ranking.js
+++ src/pages/ranking/Ranking.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './ranking.css';
+import { ReactComponent as Logo } from '../../images/trophy-solid.svg';
 
 class Ranking extends Component {
   constructor(props) {
@@ -21,30 +23,44 @@ class Ranking extends Component {
     }));
   }
 
+  class(index) {
+    if (index === 0) {
+      return 'first-place';
+    }
+    if (index === 1) {
+      return 'second-place';
+    }
+    if (index === 2) {
+      return 'third-place';
+    }
+    return 'unranked';
+  }
+
   render() {
     const { ranking } = this.state;
     return (
-      <section>
+      <section className="section-ranking">
         <h2 data-testid="ranking-title">Ranking</h2>
-        <table>
-          <thead>
-            <th>Gravatar</th>
-            <th>Nome do jogador</th>
-            <th>Pontuação</th>
-          </thead>
+        <table cellSpacing="0" cellPadding="0">
           <tbody>
             { ranking ? ranking.sort((a, b) => b.score - a.score)
               .map(({ name, score, picture }, index) => (
-                <tr key={ index }>
+                <tr className={ this.class(index) } key={ index }>
                   <td><img src={ picture } alt="profile" /></td>
                   <td data-testid={ `player-name-${index}` }>{name}</td>
                   <td data-testid={ `player-score-${index}` }>{score}</td>
+                  {index === 0 && (
+                    <td className="trs"><Logo fill="gray" className="gold" /></td>)}
+                  {index === 1 && (
+                    <td className="trs"><Logo className="silver" /></td>)}
+                  {index === 2 && (
+                    <td className="trs"><Logo className="bronze" /></td>)}
                 </tr>
               )) : null }
           </tbody>
         </table>
         <Link to="/">
-          <button type="button" data-testid="btn-go-home">
+          <button className="button-home" type="button" data-testid="btn-go-home">
             Home
           </button>
         </Link>
