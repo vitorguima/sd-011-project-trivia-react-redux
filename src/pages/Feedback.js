0c5e6596814ad51670eb170784f5112052c6ae1e
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-07-20 19:17:48 -0300

    requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,19 +1,35 @@
 import React from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 // import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const { assertions } = this.props;
+    const THREE = 3;
     return (
       <>
-        <p data-testid="feedback-text">Tela do feedback</p>
+        <p data-testid="feedback-text">Bem vindo a tela de feedback</p>
         <Header />
-
+        <main>
+          <h2 data-testid="feedback-text">
+            {assertions >= THREE ? 'Mandou bem!' : 'Podia ser melhor...'}
+          </h2>
+        </main>
       </>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  score: PropTypes.number,
+  assertions: PropTypes.number,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  score: state.user.score,
+  assertions: state.user.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
