c4b0c7dca15d881503b673faf7ced9b03e8ab476
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-07-19 21:45:53 -0300

    feat: redirecionando para ranking - req 16

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,9 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+import { Redirect } from 'react-router';
 import Header from '../components/Header';
+import { ranking } from '../actions';
 
 class Feedback extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      redirectToRaning: false,
+    };
+
+    this.toRankingReducer = this.toRankingReducer.bind(this);
+  }
+
+  getEmailWithHash(email) {
+    const hash = md5(email).toString();
+    return `https://www.gravatar.com/avatar/${hash}`;
+  }
+
+  toRankingReducer() {
+    const { player: { name, score, gravatarEmail }, toRanking } = this.props;
+    const picture = this.getEmailWithHash(gravatarEmail);
+    const data = { name, score, picture };
+    toRanking(data);
+    this.setState({
+      redirectToRaning: true,
+    });
+  }
+
   assertionsAnswer() {
     const { player: { assertions } } = this.props;
     const n = 3;
@@ -18,13 +45,22 @@ class Feedback extends React.Component {
 
   render() {
     const { player: { score, assertions } } = this.props;
+    const { redirectToRaning } = this.state;
+    if (redirectToRaning) return <Redirect to="/ranking" />;
     return (
       <div>
         <Header />
         <p data-testid="feedback-text">{ this.assertionsAnswer() }</p>
         <p data-testid="feedback-total-question">{ assertions }</p>
         <p data-testid="feedback-total-score">{ score }</p>
-        <Link data-testid="btn-play-again" to="/">Jogar novamente</Link>
+        <button
+          type="submit"
+          data-testid="btn-play-again"
+          onClick={ this.toRankingReducer }
+        >
+          Jogar novamente
+        </button>
+        <Link data-testid="btn-ranking" to="/ranking">Ver ranking</Link>
       </div>
     );
   }
@@ -34,6 +70,10 @@ const mapStateToProps = (state) => ({
   player: state.playerReducer,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  toRanking: (payload) => dispatch(ranking(payload)),
+});
+
 Feedback.propTypes = {
   player: PropTypes.shape({
     name: PropTypes.string,
@@ -41,6 +81,11 @@ Feedback.propTypes = {
     score: PropTypes.number,
     assertions: PropTypes.number,
   }).isRequired,
+  toRanking: PropTypes.shape({
+    name: PropTypes.string,
+    score: PropTypes.number,
+    picture: PropTypes.string,
+  }).isRequired,
 };
 
-export default connect(mapStateToProps)(Feedback);
+export default connect(mapDispatchToProps, mapStateToProps)(Feedback);
