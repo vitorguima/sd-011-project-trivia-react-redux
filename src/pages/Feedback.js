bb178f5d001fd2b33b99ea2778c2bf996afff686
Arlesson Moura <arlessonmss@gmail.com>
2021-07-22 15:00:04 -0300

    componentes 17 e 18 ainda com erros

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,6 +5,19 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  // componentDidMount() {
+  //   const { name, score, email } = this.props;
+  //   const picture = (<img
+  //     src={ `https://www.gravatar.com/avatar/${email}` }
+  //     data-testid="profile-picture"
+  //     alt="avatar's search"
+  //   />);
+  //   const previousState = JSON.parse(localStorage.getItem('ranking')) || [];
+  //   localStorage.setItem('ranking', JSON.stringify(
+  //     [...previousState, { name, score, picture }],
+  //   ));
+  // }
+
   render() {
     const { assertions, score } = this.props;
     const THREE = 3;
@@ -31,6 +44,8 @@ Feedback.propTypes = {
 }.isRequired;
 
 const mapStateToProps = (state) => ({
+  name: state.user.name,
+  email: state.user.gravatarEmail,
   score: state.user.score,
   assertions: state.user.assertions,
 });
