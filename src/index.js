ddd7175d549aee6ecd047b6d1aa6e3b719039878
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-07-24 17:53:39 -0300

    corrige o path para o index.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import './css/index.css';
+import './index.css';
 import App from './App';
 import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
