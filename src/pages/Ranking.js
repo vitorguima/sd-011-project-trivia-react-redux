9c1a1df326e135af7c5af2fece9e01cafbac0303
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-07-26 10:05:37 -0300

    Estilização da página de ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -23,18 +23,24 @@ class Ranking extends Component {
     const { rankState } = this.props;
     const sortedRank = rankState.sort((a, b) => b.score - a.score);
     return (
-      <div>
+      <div className="rankingContainer">
         <h1 data-testid="ranking-title">Ranking</h1>
         <Link to="/">
-          <button data-testid="btn-go-home" type="button">Jogar novamente</button>
+          <button
+            data-testid="btn-go-home"
+            type="button"
+            className="homePlayBtn"
+          >
+            Jogar novamente
+          </button>
         </Link>
-        <div>
+        <div className="rankPlayersContainer">
           {sortedRank.map((item, index) => (
             <div key={ index }>
               <img src={ item.picture } alt="profile" />
               <h5 data-testid={ `player-name-${index}` }>{ item.name }</h5>
               <h5 data-testid={ `player-score-${index}` }>{ item.score }</h5>
-              <hr />
+              <hr className="rankHr" />
             </div>
           ))}
         </div>
