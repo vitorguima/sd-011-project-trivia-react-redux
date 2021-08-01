9fbeda8f360ffba2a7ad575d7ae748c8622e4be0
FABIO3750 <fabnhm@hotmail.com>
2021-08-01 19:28:43 -0300

    Feito ajuste no packge.json

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,13 @@
-import { applyMiddleware, createStore, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers';
-
-const devTools = typeof window.__REDUX_DEVTOOLS_EXTENSION__ === 'undefined'
-  ? (a) => a
-  : window.__REDUX_DEVTOOLS_EXTENSION__
-&& window.__REDUX_DEVTOOLS_EXTENSION__();
-
-const composedThunk = compose(applyMiddleware(thunk),
-  devTools);
+import reducer from '../reducers';
 
 const store = createStore(
-  rootReducer, composedThunk,
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
 );
 
 export default store;
