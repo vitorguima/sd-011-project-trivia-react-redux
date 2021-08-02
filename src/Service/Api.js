92859e9dc864462660029c1276505acbfe073463
LukeCruz <enio.cruz@yahoo.com.br>
2021-08-01 22:38:03 -0300

    a

--- src/Service/Api.js
+++ src/Service/Api.js
@@ -1,12 +1,11 @@
 export async function fetchToken() {
-    const request = await fetch('https://opentdb.com/api_token.php?command=request');
-    const data = await request.json();
-    return data.token;
-  }
-  
-  export async function fetchQuestions(token) {
-    const request = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
-    const data = await request.json();
-    return data.results;
-  }
-  
+  const request = await fetch('https://opentdb.com/api_token.php?command=request');
+  const data = await request.json();
+  return data.token;
+}
+
+export async function fetchQuestions(token) {
+  const request = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+  const data = await request.json();
+  return data.results;
+}
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -19,78 +19,65 @@ class Feedback extends Component {
     const numberThree = 3;
     const { assertions } = this.props;
     if (assertions < numberThree) {
-        return (
-            <>
-              <img
-                src="https://i.giphy.com/media/BY8ORoRpnJDXeBNwxg/giphy.webp"
-                alt="frustrated"
-              />
-              <p data-testid="feedback-text">Podia ser melhor...</p>
-            </>
-          );
-        }
+      return (<p data-testid="feedback-text">Podia ser melhor...</p>);
+    }
+    return (<p data-testid="feedback-text">Mandou bem!</p>);
+  }
 
-        return (
-            <>
-              <img
-                src="https://i.giphy.com/media/l0amJzVHIAfl7jMDos/200.webp"
-                alt="yay"
-              />
-              <p data-testid="feedback-text">Mandou bem!</p>
-            </>
-          );
-        }
-      
-        updateRanking() {
-          const { name, score, userImageId } = this.props;
-          if (name.length !== 0) {
-            const userObj = {
-              name,
-              score,
-              picture: `https://www.gravatar.com/avatar/${userImageId}`,
-            };
-            const ranking = localStorage.getItem('ranking');
-            if (ranking === null) {
-              localStorage.setItem('ranking', JSON.stringify([userObj]));
-            } else {
-              const recoveredRanking = JSON.parse(ranking);
-              localStorage.setItem('ranking', JSON.stringify([...recoveredRanking, userObj]));
-            }
-          }
-        }
+  updateRanking() {
+    const { name, score, userImageId } = this.props;
+    if (name.length !== 0) {
+      const userObj = {
+        name,
+        score,
+        picture: `https://www.gravatar.com/avatar/${userImageId}`,
+      };
+      const ranking = localStorage.getItem('ranking');
+      if (ranking === null) {
+        localStorage.setItem('ranking', JSON.stringify([userObj]));
+      } else {
+        const recoveredRanking = JSON.parse(ranking);
+        localStorage.setItem('ranking', JSON.stringify([...recoveredRanking, userObj]));
+      }
+    }
+  }
 
-        render() {
-          const { score, assertions } = this.props;
-          return (
-            <div className="feedback-container">
-            <Header />
-            <main className="feedback-main-page">
-          { this.menssageFeedBack() }
-          <p>
-            <span className="label">Final Score: </span>
-            <span data-testid="feedback-total-score">
-              {score}
-            </span>
-          </p>
-          <p>
-            <span className="label">Right Answers: </span>
-            <span data-testid="feedback-total-question">
-              {assertions}
-            </span>
-          </p>
-          <div className="feedback-buttons-container">
-            <Link to="/">
-              <button type="button" data-testid="btn-play-again">
-                Play Again
-              </button>
-            </Link>
-            <Link to="/ranking">
-              <button type="button" data-testid="btn-ranking">
-                Ranking
-              </button>
-            </Link>
-          </div>
-        </main>
+  render() {
+    const { score, assertions } = this.props;
+    return (
+      <div>
+        <Header />
+        {this.menssageFeedBack()}
+        <p>
+          Pontuação final:
+          <span data-testid="feedback-total-score">
+            {score}
+          </span>
+        </p>
+        <p>
+          Total acertos:
+          <span
+            data-testid="feedback-total-question"
+          >
+            {assertions}
+          </span>
+        </p>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-play-again"
+          >
+            Jogar Novamente
+          </button>
+        </Link>
+        <Link to="/ranking">
+          <button
+            type="button"
+            data-testid="btn-ranking"
+          >
+            Ver Ranking
+          </button>
+        </Link>
       </div>
     );
   }
