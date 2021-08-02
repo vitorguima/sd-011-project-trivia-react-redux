00a7f3c70dc5760e88ea2fef88a9170559e703ee
Luana Souza <luanah.souzza@hotmail.com>
2021-08-01 22:27:41 -0300

    Corrige erro Linter

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,62 +11,55 @@ class Login extends Component {
       email: '',
     };
   }
-
-  async requestToken() {
-    const { props: { requestQuestions },
-      state: { name, email } } = this;
-    const state = {
-      player: {
-        name,
-        assertions: 0,
-        score: 0,
-        email,
-      },
-    };
   
-  }
-
-  isEnabled() {
-    const { state: { name, email } } = this;
-    if (name.length > 0 && email.length > 0) {
-      this.setState((prev) => ({
-        ...prev,
-        disabled: false,
-      }));
-    } 
-  }
-
-  handleChange({ target: { id, value } }) {
-    this.setState((prev) => ({
-      ...prev, [id]: value,
-    }), () => this.isEnabled());
-  }
-
-  render() {
-    const { state: { disabled },handleChange } = this;
-    return (
-        <form>
-          <label htmlFor="name">
-            Name:
-            <input
-              onChange={ handleChange }
-              data-testid="input-player-name"
-              id="name"
-              type="text"
-            />
-          </label>
-          <label htmlFor="email">
-            Email:
-            <input
-              onChange={ handleChange }
-              data-testid="input-gravatar-email"
-              id="email"
-              type="email"
-            />
-          </label>
-          <button disabled={ disabled } data-testid="btn-play" type="button">Jogar</button>
-        </form>
+    isEnabled() {
+      const { state: { name, email } } = this;
+      if (name.length > 0 && email.length > 0) {
+        this.setState((prev) => ({
+          ...prev,
+          disabled: false,
+        }));
+      } else {
+        this.setState((prev) => ({
+          ...prev,
+          disabled: true,
+        }));
+      }
+    }
+  
+    handleChange({ target: { id, value } }) {
+          this.setState((prev) => ({
+            ...prev, [id]: value,
+          }), () => this.isEnabled());
+        }
+      
+        render() {
+              const { state: { disabled }, handleChange } = this;
+              return (
+                <form>
+                  <label htmlFor="name">
+                    Name:
+                    <input
+                      onChange={ handleChange }
+                      data-testid="input-player-name"
+                      id="name"
+                      type="text"
+                    />
+                  </label>
+                  <label htmlFor="email">
+                    Email:
+                    <input
+                      onChange={ handleChange }
+                      data-testid="input-gravatar-email"
+                      id="email"
+                      type="email"
+          />
+        </label>
+        <button disabled={ disabled } data-testid="btn-play" type="button">Jogar</button>
+      </form>
     );
   }
 }
+
 export default Login;
+                      
