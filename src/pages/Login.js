75e6ccb3160a02925126aa5d9b3bd2b95c10bd49
Luana Souza <luanah.souzza@hotmail.com>
2021-08-01 22:40:50 -0300

    Corrige indentaçao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,50 +11,48 @@ class Login extends Component {
       email: '',
     };
   }
-    isEnabled() {
+  isEnabled() {
     const { state: { name, email } } = this;
     if (name.length > 0 && email.length > 0) {
-        this.setState((prev) => ({
-          ...prev,
-          disabled: false,
-        }));
-      } else {
-        this.setState((prev) => ({
-          ...prev,
-          disabled: true,
-        }));
-      }
+      this.setState((prev) => ({
+        ...prev,
+        disabled: false,
+      }));
+    } else {
+      this.setState((prev) => ({
+        ...prev,
+        disabled: true,
+      }));
     }
-  
-    handleChange({ target: { id, value } }) {
-          this.setState((prev) => ({
-            ...prev, [id]: value,
-          }), () => this.isEnabled());
-        }
-      
-        render() {
-              const { state: { disabled }, handleChange } = this;
-              return (
-                <form>
-                  <label htmlFor="name">
-                    Name:
-                    <input
-                      onChange={ handleChange }
-                      data-testid="input-player-name"
-                      id="name"
-                      type="text"
-                    />
-                  </label>
-                  <label htmlFor="email">
-                    Email:
-                    <input
-                      onChange={ handleChange }
-                      data-testid="input-gravatar-email"
-                      id="email"
-                      type="email"
-          />
-        </label>
-        <button disabled={ disabled } data-testid="btn-play" type="button">Jogar</button>
+  }
+  handleChange({ target: { id, value } }) {
+        this.setState((prev) => ({
+          ...prev, [id]: value,
+        }), () => this.isEnabled());
+      }
+      render() {
+            const { state: { disabled }, handleChange } = this;
+            return (
+              <form>
+                <label htmlFor="name">
+                  Name:
+                  <input
+                    onChange={ handleChange }
+                    data-testid="input-player-name"
+                    id="name"
+                    type="text"
+                  />
+                </label>
+                <label htmlFor="email">
+                  Email:
+                  <input
+                    onChange={ handleChange }
+                    data-testid="input-gravatar-email"
+                    id="email"
+                    type="email"
+        />
+      </label>
+      <button disabled={ disabled } data-testid="btn-play" type="button">Jogar</button>
       </form>
     );
   }
